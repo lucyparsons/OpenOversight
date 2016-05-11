768d48cce0825dee12e57e2e6ff9281cbe13d137
redshiftzero <jen@redshiftzero.com>
2016-05-11 08:13:56 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="OpenOversight", version="1.0",
+      description="Oversight of Police Departments",
+      author="redshiftzero", author_email="jen@redshiftzero.com",
+      install_requires=['flask', 'werkzeug', 'Flask-WTF']
+     )
