fbefa7c36befa9ff16305dd4193cf525e85059e6
redshiftzero <jen@redshiftzero.com>
2016-05-11 08:14:13 -0500

    Add example config

---
+++ OpenOversight/config.py
@@ -0,0 +1,7 @@
+# File Upload Settings
+UNLABELLED_UPLOADS = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'mpeg', 'mp4'])
+
+# Form Settings
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'changemeplzorelsehax'
