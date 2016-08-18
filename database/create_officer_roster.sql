63cbe04332edd3a6463abf8739a90e2b20daa877
redshiftzero <jen@redshiftzero.com>
2016-08-18 00:11:28 -0500

    Add new schema and table creation scripts

---
+++ database/create_officer_roster.sql
@@ -0,0 +1,12 @@
+drop table if exists officers.roster;
+
+create table officers.roster (
+        officer_id serial primary key,
+        last_name varchar(80),
+        first_name varchar(80),
+        middle_initial varchar(10),
+        race varchar(40),
+        gender varchar(40),
+        employment_date date,
+        birth_year integer
+);
---
+++ database/create_officer_schema.sql
@@ -0,0 +1,2 @@
+DROP SCHEMA IF EXISTS officers CASCADE;
+CREATE SCHEMA officers;
