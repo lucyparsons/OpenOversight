57e1842bc2ca80fb810f44a7f4ad5952597b7b0a
redshiftzero <jen@redshiftzero.com>
2016-08-20 17:38:29 -0500

    Add raw images table

--- database/create_officer_faces.sql
+++ database/create_officer_faces.sql
@@ -1,8 +1,8 @@
 drop table if exists officers.faces;
 
 create table officers.faces (
-        img_id serial primary key,
-        img_path varchar(200),
+        tag_id serial primary key,
+        img_id integer references officers.raw_images (img_id),
         officer_id integer references officers.roster (officer_id),
         face_position box
 );
