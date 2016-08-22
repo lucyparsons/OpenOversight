586391175b38c83d2db3ff5f97af17d8204cfb0d
Freddy Martinez <freddymartinez9@gmail.com>
2016-08-22 03:08:52 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name="OpenOversight", version="1.0",
       description="Oversight of Police Departments",
       author="redshiftzero", author_email="jen@redshiftzero.com",
-      install_requires=['flask', 'werkzeug', 'Flask-WTF']
+      install_requires=['flask', 'werkzeug', 'Flask-WTF', 'psycopg2', 'sqlalchemy']
      )
