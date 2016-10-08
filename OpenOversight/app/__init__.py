5759b3d4f2aad335d02bb96939d6835ad95770ac
redshiftzero <jen@redshiftzero.com>
2016-10-08 16:05:46 -0700

    Update __init__ to use new Models

--- OpenOversight/app/__init__.py
+++ OpenOversight/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
-from app import utils
+from app import views, models, utils
