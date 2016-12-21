8baf5a66e80793c2c7a740794be70a4bda1428ab
redshiftzero <jen@redshiftzero.com>
2016-12-20 17:54:02 -0800

    Add custom error handlers to base app

--- OpenOversight/app/__init__.py
+++ OpenOversight/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
@@ -11,6 +11,18 @@ def create_app(config_name='default'):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('403.html'), 403
+
+    @app.errorhandler(500)
+    def internal_error(e):
+        return render_template('500.html'), 500
 
     return app
 
