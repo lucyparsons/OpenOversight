a6995e7bb4987a7dd87f0c3ea28d59daa7592848
redshiftzero <jen@redshiftzero.com>
2016-12-24 13:28:26 -0800

    Werkzeug is using SHA1 for password hashing, let's use SHA256

--- OpenOversight/app/models.py
+++ OpenOversight/app/models.py
@@ -101,7 +101,8 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password,
+                                                    method='pbkdf2:sha256')
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
