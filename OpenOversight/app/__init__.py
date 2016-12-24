11d9059bd07299e4760b4e9768df02d80e1cb32c
redshiftzero <jen@redshiftzero.com>
2016-12-24 13:02:14 -0800

    Add auth blueprint and Flask-Bootstrap init

--- OpenOversight/app/__init__.py
+++ OpenOversight/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
+
+bootstrap = Bootstrap()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -13,10 +18,18 @@ def create_app(config_name='default'):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     from .models import db
+
+    bootstrap.init_app(app)
+    #mail.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
 
     @app.errorhandler(404)
     def page_not_found(e):
