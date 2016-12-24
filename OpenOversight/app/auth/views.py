6c1b2423b55d7cc716a434ce7e5c297b82107e6b
redshiftzero <jen@redshiftzero.com>
2016-12-24 13:01:49 -0800

    Fix path to database

--- OpenOversight/app/auth/views.py
+++ OpenOversight/app/auth/views.py
@@ -2,8 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, \
     current_user
 from . import auth
-from .. import db
-from ..models import User
+from ..models import User, db
 from ..email import send_email
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
     PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
