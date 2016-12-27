c84ca46dac1de594013cbcb79ba2bbf726c4aff5
redshiftzero <jen@redshiftzero.com>
2016-12-27 02:20:29 +0100

    Set minimum password length to 8 chars

--- OpenOversight/app/auth/forms.py
+++ OpenOversight/app/auth/forms.py
@@ -17,7 +17,7 @@ class RegistrationForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
                                            Email()])
     username = StringField('Username', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+        Required(), Length(8, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Usernames must have only letters, '
                                           'numbers, dots or underscores')])
     password = PasswordField('Password', validators=[
