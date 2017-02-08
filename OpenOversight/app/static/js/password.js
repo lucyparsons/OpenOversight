cfa39cb9f58362b4c947d973102a9231aadec9fe
Tek <tek@randhome.io>
2017-02-07 19:35:14 -0500

    improve password strength validation client side

--- OpenOversight/app/static/js/password.js
+++ OpenOversight/app/static/js/password.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-    var strength = {
-        0: "Very Bad",
-        1: "Bad",
-        2: "Weak",
-        3: "Good",
-        4: "Strong"
-    }
+    var emailfield = document.getElementById('email');
+    var userfield = document.getElementById('username');
+    var oldpasswdfield = document.getElementById('old_password');
     var password = document.getElementById('password');
     var password2 = document.getElementById('password2');
     var meter = document.getElementById('password-strength-meter');
     var meter2 = document.getElementById('password-confirmation-meter');
     var text = document.getElementById('password-strength-text');
+    var submit = $('#password-button');
 
     function check_confirmation() {
         var val1 = password.value;
@@ -19,13 +16,41 @@ $(document).ready(function() {
             if (val1 == val2) {
                 meter2.value = 5;
                 $(meter2).removeClass("badmeter").addClass("goodmeter");
+                return true ;
             } else {
                 meter2.value = 5;
                 $(meter2).removeClass("goodmeter").addClass("badmeter");
+                return false ;
             }
         } else {
             meter2.value = 0;
+            return false ;
         }
+    }
+
+    function validate_form() {
+        valid = true ;
+        if ((emailfield != undefined) && (emailfield.value == "" )) {
+            valid = false ;
+        }
+        if ((userfield != undefined) && (userfield.value == "")) {
+            valid = false ;
+        }
+        if ((oldpasswdfield != undefined) && (oldpasswdfield.value == "")) {
+            valid = false ;
+        }
+        if (meter.value < 3) {
+            valid = false ;
+        }
+        if (!check_confirmation()) {
+            valid = false ;
+        }
+        if (valid) {
+            submit.attr("disabled", false);
+        } else {
+            submit.attr("disabled", true);
+        }
+        return valid ;
     }
 
     password.addEventListener('input', function() {
@@ -33,14 +58,33 @@ $(document).ready(function() {
         var result = zxcvbn(val);
         meter.value = result.score +1;
         if (val !== "") {
-            text.innerHTML = "Strength: " + strength[result.score];
+            if (meter.value < 3) {
+                text.innerHTML = "Password not strong enough";
+            } else {
+                text.innerHTML = "OK";
+            }
         } else {
             text.innerHTML = "";
             meter.value = 0;
         }
-        check_confirmation();
+        validate_form();
     });
     password2.addEventListener('input', function() {
-        check_confirmation();
+        validate_form();
     });
+    if (emailfield != undefined) {
+        emailfield.addEventListener('input', function() {
+            validate_form();
+        });
+    };
+    if (userfield != undefined) {
+        userfield.addEventListener('input', function() {
+            validate_form();
+        });
+    };
+    if (oldpasswdfield != undefined) {
+        oldpasswdfield.addEventListener('input', function() {
+            validate_form();
+        });
+    };
 });
