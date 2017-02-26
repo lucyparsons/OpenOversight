70bdab58a8c2bf343bb1e68c3d3d8b341930e247
redshiftzero <jen@redshiftzero.com>
2017-02-25 22:42:50 -0800

    Remove db migration from coverage computation

--- OpenOversight/app/db_repository/versions/002_migration.py
+++ OpenOversight/app/db_repository/versions/002_migration.py
@@ -1,11 +1,11 @@
-from sqlalchemy import *
-from migrate import *
+from sqlalchemy import *  # pragma: no cover
+from migrate import *  # pragma: no cover
 
 
-from migrate.changeset import schema
-pre_meta = MetaData()
-post_meta = MetaData()
-raw_images = Table('raw_images', post_meta,
+from migrate.changeset import schema  # pragma: no cover
+pre_meta = MetaData()  # pragma: no cover
+post_meta = MetaData()  # pragma: no cover
+raw_images = Table('raw_images', post_meta,  # pragma: no cover
     Column('id', Integer, primary_key=True, nullable=False),
     Column('filepath', String(length=255)),
     Column('hash_img', String(length=120)),
@@ -17,7 +17,7 @@ raw_images = Table('raw_images', post_meta,
 )
 
 
-def upgrade(migrate_engine):
+def upgrade(migrate_engine):  # pragma: no cover
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
@@ -25,7 +25,7 @@ def upgrade(migrate_engine):
     post_meta.tables['raw_images'].columns['filepath'].alter(type=String(255))
 
 
-def downgrade(migrate_engine):
+def downgrade(migrate_engine):  # pragma: no cover
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
