7d730c7c8c76aecd3eade57d62a33752f1eb92aa
redshiftzero <jen@redshiftzero.com>
2017-03-04 12:35:19 -0800

    Cleanup tables after rolling back the transaction but before closing the connection

--- OpenOversight/tests/conftest.py
+++ OpenOversight/tests/conftest.py
@@ -111,16 +111,18 @@ def session(db, request):
     db.session = session
 
     def teardown():
+        transaction.rollback()
+        session.remove()
+
         # Cleanup tables
         models.User.query.delete()
         models.Officer.query.delete()
         models.Image.query.delete()
         models.Face.query.delete()
         session.commit()
+        session.flush()
 
-        transaction.rollback()
         connection.close()
-        session.remove()
 
     request.addfinalizer(teardown)
     return session
