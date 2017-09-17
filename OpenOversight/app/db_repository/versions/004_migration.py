70e1103b760c7546f312c7636d4394f6a913c511
redshiftzero <jen@freedom.press>
2017-09-17 00:06:31 -0700

    Link all images to the first police department
    * department_id is not a foreign key (yet) as the schema migration
    was tricky. I need to set the column to a NOT NULL value first
    and then at a later date can add this foreign key constraint.
    
    * Add schema migration
    
    * Add command to link all existing images to the first department

---
+++ OpenOversight/app/db_repository/versions/004_migration.py
@@ -0,0 +1,35 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+
+
+raw_images = Table('raw_images', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('filepath', String(length=255)),
+    Column('hash_img', String(length=120)),
+    Column('date_image_inserted', DateTime),
+    Column('date_image_taken', DateTime),
+    Column('contains_cops', Boolean),
+    Column('user_id', Integer),
+    Column('is_tagged', Boolean, default=ColumnDefault(False)),
+    Column('department_id', Integer),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['raw_images'].columns['department_id'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['raw_images'].columns['department_id'].drop()
