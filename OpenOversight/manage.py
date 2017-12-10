ad8463e21783448838ed0777411489ba2b02d9b6
redshiftzero <jen@freedom.press>
2017-12-09 20:11:23 -0800

    Rework manage.py to use Flask-Migrate's db command

--- OpenOversight/manage.py
+++ OpenOversight/manage.py
@@ -1,17 +1,19 @@
 from getpass import getpass
 import imp
-from migrate.versioning import api
 import sys
 
 from flask_script import Manager, Server, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 from app import app
 from app.models import db, User
 from app.config import config
 
 
+migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command("runserver", Server(host="0.0.0.0", port=3000))
+manager.add_command("db", MigrateCommand)
 
 
 def make_shell_context():
@@ -19,49 +21,6 @@ def make_shell_context():
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
-@manager.command
-def makemigrations(config_name="default"):
-    """Make database migrations"""
-
-    SQLALCHEMY_MIGRATE_REPO = config['default'].SQLALCHEMY_MIGRATE_REPO
-    SQLALCHEMY_DATABASE_URI = config['default'].SQLALCHEMY_DATABASE_URI
-
-    v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    migration = SQLALCHEMY_MIGRATE_REPO + ('/versions/%03d_migration.py' % (v + 1))
-    tmp_module = imp.new_module('old_model')
-    old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    exec(old_model, tmp_module.__dict__)
-    script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
-    open(migration, "wt").write(script)
-
-    print('New migration saved as ' + migration)
-    print('Run python manage.py upgrade_db to upgrade the database')
-
-
-@manager.command
-def migrate(config_name="default"):
-    """Migrate/upgrade the database"""
-
-    SQLALCHEMY_MIGRATE_REPO = config['default'].SQLALCHEMY_MIGRATE_REPO
-    SQLALCHEMY_DATABASE_URI = config['default'].SQLALCHEMY_DATABASE_URI
-    api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    print('Current database version: ' + str(v))
-
-
-@manager.command
-def downgrade_db(config_name="default"):
-    """Downgrade the database"""
-
-    SQLALCHEMY_MIGRATE_REPO = config['default'].SQLALCHEMY_MIGRATE_REPO
-    SQLALCHEMY_DATABASE_URI = config['default'].SQLALCHEMY_DATABASE_URI
-
-    v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
-    v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    print('Current database version: ' + str(v))
 
 
 @manager.command
