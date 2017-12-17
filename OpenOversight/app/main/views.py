9077f35ea6e6bac879c57b9136d00835da8e7490
Larissa Marcich <lmarcich@thoughtworks.com>
2017-12-16 20:34:01 -0600

    Update flash to match current use

--- OpenOversight/app/main/views.py
+++ OpenOversight/app/main/views.py
@@ -72,7 +72,7 @@ def get_started_labeling():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password', 'error')
+        flash('Invalid username or password.')
     departments = Department.query.all()
     return render_template('label_data.html', departments=departments, form=form)
 
