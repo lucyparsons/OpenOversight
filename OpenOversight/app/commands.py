806fb75908a8f713c0b6cb876faf6a6bcb37e10a
Travis Cline <travis.cline@gmail.com>
2018-02-17 21:57:51 -0500

    manage.py: add bulk upload command

--- OpenOversight/app/commands.py
+++ OpenOversight/app/commands.py
@@ -2,12 +2,13 @@ from __future__ import print_function
 from builtins import input
 from getpass import getpass
 import sys
+import csv
 
 import click
 from flask.cli import with_appcontext
-from flask import current_app
 
-from .models import db, User
+from .models import db, Assignment, Department, Officer, User
+from .utils import badge_exists
 
 
 # @manager.command
@@ -44,8 +45,8 @@ def make_admin_user():
     db.session.add(u)
     db.session.commit()
     print("Administrator {} successfully added".format(username))
-    current_app.logger.info('Administrator {} added with email {}'.format(username,
-                                                                          email))
+    app.logger.info('Administrator {} added with email {}'.format(username,
+                                                                  email))
 
 
 # @manager.command
@@ -83,3 +84,49 @@ def link_officers_to_department():
         else:
             print("Skipped! Object already assigned to department!")
     db.session.commit()
+
+
+# @manager.command
+@click.command()
+@with_appcontext
+def bulk_add_officers(filename):
+    """Bulk adds officers."""
+    csvfile = csv.DictReader(open(filename))
+    departments = {}
+    n_created = 0
+    for line in csvfile:
+        department_id = line['department_id']
+        department = departments.get(department_id)
+        if line['department_id'] not in departments:
+            department = Department.query.filter_by(id=department_id).one_or_none()
+            if department:
+                departments[department_id] = department
+            else:
+                raise Exception('Department ID {} not found'.format(department_id))
+
+        # check for existing officer based on badge
+        if badge_exists(line['badge'], department_id):
+            print 'Skipping creation of existing badge id {} for department id {}'.format(line['badge'], department_id)
+            continue
+
+        # create officer
+        officer = Officer()
+        officer.department_id = department_id
+        officer.last_name = line['last_name']
+        officer.first_name = line['first_name']
+        officer.middle_initial = line['middle_initial']
+        officer.race = line['race']
+        officer.gender = line['gender']
+        officer.employment_date = line['employment_date']
+        officer.birth_year = line['birth_year']
+        db.session.add(officer)
+        db.session.commit()
+
+        assignment = Assignment()
+        assignment.officer_id = officer.id
+        assignment.star_no = line['badge']
+        assignment.rank = line['rank']
+        db.session.add(assignment)
+        db.session.commit()
+        n_created += 1
+    print 'created {} officers'.format(n_created)
