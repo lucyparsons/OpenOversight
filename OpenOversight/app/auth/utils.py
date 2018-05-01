58184d0e211a598d7367a25e08f12e3de7410e14
racheltstevens <racheltstevens@gmail.com>
2018-05-01 12:57:28 -0400

    Create ac_or_admin decorator
    Checks that user is an area coordinator or administrator

--- OpenOversight/app/auth/utils.py
+++ OpenOversight/app/auth/utils.py
@@ -2,10 +2,23 @@ from functools import wraps
 from flask import abort
 from flask_login import current_user
 
+
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.is_administrator:
             abort(403)
         return f(*args, **kwargs)
+    return decorated_function
+
+
+def ac_or_admin_required(f):
+    """Decorate that requires that the user be an area coordinator or administrator"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not (current_user.is_administrator or
+                current_user.is_area_coordinator):
+            abort(403)
+        return f(*args, **kwargs)
+
     return decorated_function
