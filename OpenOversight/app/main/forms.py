2f05f730a40d0081a35be1afe1a50fc557a87c5d
racheltstevens <racheltstevens@gmail.com>
2018-05-01 16:41:09 -0400

    Move constants to their own file

--- OpenOversight/app/main/forms.py
+++ OpenOversight/app/main/forms.py
@@ -9,22 +9,7 @@ from wtforms.validators import (DataRequired, AnyOf, NumberRange, Regexp,
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 from ..utils import unit_choices, dept_choices
-
-
-# Choices are a list of (value, label) tuples
-RACE_CHOICES = [('BLACK', 'Black'), ('WHITE', 'White'), ('ASIAN', 'Asian'),
-                ('HISPANIC', 'Hispanic'),
-                ('NATIVE AMERICAN', 'Native American'),
-                ('PACIFIC ISLANDER', 'Pacific Islander'),
-                ('Other', 'Other'), ('Not Sure', 'Not Sure')]
-GENDER_CHOICES = [('M', 'Male'), ('F', 'Female'), ('Other', 'Other'),
-                  ('Not Sure', 'Not Sure')]
-RANK_CHOICES = [('Not Sure', 'Not Sure'), ('SUPT OF POLICE', 'Superintendent'),
-                ('DEPUTY SUPT', 'Deputy Superintendent'), ('CHIEF', 'Chief'),
-                ('DEP CHIEF', 'Deputy Chief'), ('COMMANDER', 'Commander'),
-                ('CAPTAIN', 'Captain'), ('LIEUTENANT', 'Lieutenant'),
-                ('SERGEANT', 'Sergeant'), ('FIELD', 'Field Training Officer'),
-                ('PO', 'Police Officer')]
+from .choices import GENDER_CHOICES, RACE_CHOICES, RANK_CHOICES
 
 
 def allowed_values(choices):
