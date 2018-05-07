d674e279a70ded681efa5a6cbec1e1ffc1e27cb3
racheltstevens <racheltstevens@gmail.com>
2018-05-07 17:32:15 -0400

    Update admin required functions
    Properly return 403 instead of erroring when the user is anonymous

--- OpenOversight/app/auth/utils.py
+++ OpenOversight/app/auth/utils.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not current_user.is_administrator:
+        if current_user.is_anonymous or not current_user.is_administrator:
             abort(403)
         return f(*args, **kwargs)
     return decorated_function
@@ -16,7 +16,8 @@ def ac_or_admin_required(f):
     """Decorate that requires that the user be an area coordinator or administrator"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not (current_user.is_administrator or
+        if current_user.is_anonymous or \
+           not (current_user.is_administrator or
                 current_user.is_area_coordinator):
             abort(403)
         return f(*args, **kwargs)
