7efcef64a1dd1073ec42c310531d7d7492b7879b
redshiftzero <jen@freedom.press>
2018-05-27 15:53:50 -0700

    Race, gender cannot have Nones

--- test_data.py
+++ test_data.py
@@ -30,11 +30,11 @@ def pick_birth_date():
 
 def pick_race():
     return random.choice(['WHITE', 'BLACK', 'HISPANIC', 'ASIAN',
-                          'PACIFIC ISLANDER', None])
+                          'PACIFIC ISLANDER', 'Not Sure'])
 
 
 def pick_gender():
-    return random.choice(['M', 'F', None])
+    return random.choice(['M', 'F', 'Not Sure'])
 
 
 def pick_first():
