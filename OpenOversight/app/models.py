2388e9f8272d6ef759eb7be1bff4388db5a42ed6
Dan Staples <dan@disman.tl>
2018-08-13 13:26:10 -0400

    Assignments should cascade delete when officers are deleted

--- OpenOversight/app/models.py
+++ OpenOversight/app/models.py
@@ -107,7 +107,7 @@ class Assignment(db.Model):
     __tablename__ = 'assignments'
 
     id = db.Column(db.Integer, primary_key=True)
-    officer_id = db.Column(db.Integer, db.ForeignKey('officers.id'))
+    officer_id = db.Column(db.Integer, db.ForeignKey('officers.id', ondelete='CASCADE'))
     baseofficer = db.relationship('Officer')
     star_no = db.Column(db.String(120), index=True, unique=False)
     rank = db.Column(db.String(120), index=True, unique=False)
