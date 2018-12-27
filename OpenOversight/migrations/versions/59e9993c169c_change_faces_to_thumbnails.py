f0de4534ea11b691e6a3e7c5e535d1d429448f17
Dan Staples <dan@disman.tl>
2018-12-27 17:12:45 -0500

    Move migration imports to specific revision files

--- OpenOversight/migrations/versions/59e9993c169c_change_faces_to_thumbnails.py
+++ OpenOversight/migrations/versions/59e9993c169c_change_faces_to_thumbnails.py
@@ -5,6 +5,11 @@ Revises: bd0398fe4aab
 Create Date: 2018-06-04 19:04:23.524079
 
 """
+from flask import current_app
+import os
+import sys
+# Add our Flask app to the search paths for modules
+sys.path.insert(0, os.path.dirname(current_app.root_path))
 
 from app.models import Face, db
 from app.utils import get_uploaded_cropped_image
