cda6fcda91479c374ce336a2b43ab32a73a2f02b
Dan Staples <dan@disman.tl>
2018-12-29 12:10:29 -0500

    Allow unique internal identifier to be None when editing officer profile

--- OpenOversight/app/main/forms.py
+++ OpenOversight/app/main/forms.py
@@ -213,7 +213,10 @@ class EditOfficerForm(Form):
                          validators=[AnyOf(allowed_values(GENDER_CHOICES))])
     employment_date = DateField('Employment Date', validators=[Optional()])
     birth_year = IntegerField('Birth Year', validators=[Optional()])
-    unique_internal_identifier = StringField('Unique Internal Identifier', default='', validators=[Regexp('\w*'), Length(max=50)])
+    unique_internal_identifier = StringField('Unique Internal Identifier',
+                                             default='',
+                                             validators=[Regexp('\w*'), Length(max=50)],
+                                             filters=[lambda x: x or None])
     department = QuerySelectField(
         'Department',
         validators=[Optional()],
