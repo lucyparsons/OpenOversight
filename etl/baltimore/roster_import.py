d79fb0c332318161014e524d3341a030a66ccfbe
Dan Staples <dan@disman.tl>
2019-01-28 11:38:54 -0500

    Updated baltimore roster import script

--- etl/baltimore/roster_import.py
+++ etl/baltimore/roster_import.py
@@ -1,21 +1,41 @@
 import pandas as pd
 import os
 import sys
+import csv
+from datetime import datetime
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from OpenOversight.app import create_app, models
-from OpenOversight.app.models import db
+from OpenOversight.app import create_app, models  # noqa E402
+from OpenOversight.app.models import db  # noqa E402
 
 app = create_app('development')
 db.app = app
+
+fieldnames = [
+    'department_id',
+    'unique_internal_identifier',
+    'first_name',
+    'last_name',
+    'middle_initial',
+    'suffix',
+    'gender',
+    'race',
+    'employment_date',
+    'birth_year',
+    'star_no',
+    'rank',
+    'unit',
+    'star_date',
+    'resign_date'
+]
 
 columns = {
     # 'Location': 'location',
     'EMPLID': 'employee_id',
     'Last Name': 'last_name',
     'First Name': 'first_name',
-    # 'Middle Name': 'middle_initial',
+    'Middle Name': 'middle_initial',
     'SEX': 'sex',
     'Ethnic Group': 'ethnic_group',
     'Service Date': 'service_date',
@@ -29,14 +49,20 @@ columns = {
     'Grade': 'grade',
     'GL Pay Type': 'gl_pay_type',
     # 'Locatlity': 'locality', # sic
-    'SEQ#': 'seq_no'
+    'SEQ# (A99 only)': 'seq_no'
+    # 'SEQ#': 'seq_no'
 }
 
+
 def load(filename):
-    print("Importing raw roster")
+    print("Importing raw roster", filename)
     df = pd.read_excel(filename)
     for column in columns.keys():
-        assert column in df.columns
+        try:
+            assert column in df.columns
+        except AssertionError:
+            print('Did not find column {} in roster'.format(column))
+            raise
     df.rename(columns=columns, inplace=True)
     try:
         df.to_sql('roster_raw', db.engine)
@@ -54,43 +80,70 @@ def load(filename):
     if not bpd:
         print("Creating department")
         bpd = models.Department(name='Baltimore City Police Department',
-            short_name='BPD')
+                                short_name='BPD')
         db.session.add(bpd)
         db.session.commit()
 
-    print("Deleting previous officer records")
-    db.session.execute(
-        models.Officer.__table__.delete().where(models.Officer.department_id == bpd.id)
-    )
-    db.session.commit()
+    with open('export.csv', 'w', newline='') as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=fieldnames, extrasaction='ignore')
+        writer.writeheader()
+        for pig in df.itertuples():
+            print(pig.seq_no)
+            assignment_date = None
+            if pig.promotion_date and pig.promotion_date != ' ' and type(pig.promotion_date) != float:
+                if type(pig.promotion_date) == str:
+                    assignment_date = datetime.strptime(pig.promotion_date, '%m/%d/%Y').date()
+                else:
+                    assignment_date = pig.promotion_date.date()
+            elif pig.rehire_date and pig.rehire_date != ' ' and type(pig.rehire_date) != float:
+                if type(pig.rehire_date) == str:
+                    assignment_date = datetime.strptime(pig.rehire_date, '%m/%d/%Y').date()
+                else:
+                    assignment_date = pig.rehire_date.date()
 
-    print("Importing new officer records")
-    for pig in df.itertuples():
-        # print(pig.seq_no)
-        try:
-            officer = models.Officer(
-                department_id = bpd.id,
-                last_name = pig.last_name,
-                first_name = pig.first_name,
-                race = pig.ethnic_group,
-                # middle_initial = pig.middle_initial.strip() if type(pig.middle_initial) == unicode else None,
-                gender = pig.sex,
-                employment_date = pig.service_date,
-                unique_internal_identifier = pig.seq_no
-            )
-        except:
-            import pdb; pdb.set_trace()
-        db.session.add(officer)
-        db.session.commit()
+            if pig.service_date and pig.service_date != ' ' and type(pig.service_date) != float:
+                if type(pig.service_date) == str:
+                    employment_date = datetime.strptime(pig.service_date, '%m/%d/%Y').date()
+                else:
+                    employment_date = pig.service_date.date()
+                if not assignment_date:
+                    assignment_date = employment_date
 
-        assignment = models.Assignment(
-            officer_id = officer.id,
-            rank = pig.job_title
-        )
-        db.session.add(assignment)
+            try:
+                rint = int(pig.ethnic_group)
+            except ValueError:
+                race = pig.ethnic_group
+            else:
+                if rint == 1:
+                    race = 'White'
+                elif rint == 2:
+                    race = 'Black or African American'
+                elif rint == 3:
+                    race = 'Hispanic'
+                elif rint == 4:
+                    race = 'Asian/Pacific Islander'
+                elif rint == 5:
+                    race = 'American Indian/Alaska Native'
+                elif rint == 6:
+                    race = 'Not Applicable (Non-U.S.)'
+                elif rint == 7:
+                    race = 'Not Specified'
 
-    db.session.commit()
+            writer.writerow({
+                'department_id': bpd.id,
+                'unique_internal_identifier': pig.seq_no,
+                'first_name': pig.first_name,
+                'middle_initial': pig.middle_initial if type(pig.middle_initial) == str else None,
+                'last_name': pig.last_name,
+                'gender': pig.sex,
+                'race': race,
+                'employment_date': employment_date,
+                'rank': pig.job_title,
+                'star_date': assignment_date
+            })
+
     print("Finished data import!")
 
+
 if __name__ == '__main__':
-    load('Employee_Information_for_Release_10.31.2018.xlsx')
+    load(sys.argv[1])
