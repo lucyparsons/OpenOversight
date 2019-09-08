363912c584d839121467ec1c05e96ae6f368cacb
McEileen <eileenm@gmail.com>
2019-09-08 17:53:20 -0400

    can autodetect & tag one face per photo

--- OpenOversight/app/static/js/autodetect_faces.js
+++ OpenOversight/app/static/js/autodetect_faces.js
@@ -1,31 +1,43 @@
 $(document).ready(function() {;
     const MODEL_URL = '/static/face_api_js_models'
 
-    async function load_models(){
+    async function loadModels(){
         await faceapi.loadFaceRecognitionModel(MODEL_URL)
         await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
         await faceapi.loadFaceLandmarkModel(MODEL_URL)
     }
 
-    load_models().then(result => result)
+    loadModels().then(result => result)
 
-    async function get_faces(image){
-        return await faceapi.detectAllFaces(image)
+    async function getMostConfidentFace(image){
+        return await faceapi.detectSingleFace(image)
+    }
+
+    function setCropBoundaries(boxCoordinates) {
+        const dataX = document.getElementById('dataX')
+        const dataY = document.getElementById('dataY')
+        const dataWidth = document.getElementById('dataWidth')
+        const dataHeight = document.getElementById('dataHeight')
+        dataX.value = Math.ceil(boxCoordinates.x)
+        dataY.value = Math.ceil(boxCoordinates.y)
+        dataWidth.value = Math.ceil(boxCoordinates.width)
+        dataHeight.value = Math.ceil(boxCoordinates.height)
     }
 
     image = document.getElementById('image')
     $('#autodetector').click(function(e) {
         e.preventDefault();
         const canvas = faceapi.createCanvasFromMedia(image)
-        document.body.append(canvas)
         const displaySize = { width: image.width, height: image.height }
-        console.log('displaySize', displaySize)
-        get_faces(image)
-            .then((face_detections) => {
-                face_detections.forEach(face_detection => console.log(face_detection))
-                console.log('face_detections', face_detections)
-                const resizedDetections = faceapi.resizeResults(face_detections, displaySize)
-                faceapi.draw.drawDetections(canvas, resizedDetections)
+        getMostConfidentFace(image)
+            .then((faceDetection) => {
+                const resizedDetection = faceapi.resizeResults(faceDetection, displaySize)
+                const cropperSpace = document.getElementById('image-with-cropper')
+                const autoDetection = document.getElementById('autodetection')
+                cropperSpace.style.display = "none" 
+                autoDetection.replaceWith(canvas)
+                faceapi.draw.drawDetections(canvas, resizedDetection)
+                setCropBoundaries(resizedDetection.box)
             })
             .catch(error => console.log(error))
         });
