39787ec915e130b8f7976839091e241bdb9a55b8
Mason Donahue <masond@gmail.com>
2020-08-08 20:25:47 -0500

    Confirm the confirmed users, allow fake email for devs
    The previous code path was missing a commit() in the db session that marked users as confirmed when accessing the /auth/confirm/<token> route, so add that.
    
    Fixes #762 (hopefully)

--- OpenOversight/app/email.py
+++ OpenOversight/app/email.py
@@ -10,11 +10,14 @@ def send_async_email(app, msg):
 
 
 def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
-    msg = Message(app.config['OO_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['OO_MAIL_SENDER'], recipients=[to])
+    msg = Message(current_app.config['OO_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=current_app.config['OO_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    # Only send email if we're in prod or staging, otherwise log it so devs can see it
+    if current_app.env in ("staging", "production"):
+        thr = Thread(target=send_async_email, args=[current_app, msg])
+        thr.start()
+        return thr
+    else:
+        current_app.logger.info("simulated email:\n%s\n%s", subject, msg.body)
