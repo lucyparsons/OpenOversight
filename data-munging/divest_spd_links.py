ed3abf27eeb7ed175e26f6a374359daa8d9d0ca8
Madison Swain-Bowden <bowdenm@spu.edu>
2021-09-01 09:42:17 -0700

    Munger: Twitter Links (#39)
    * Add justfile
    
    * Add data munging script
    
    * Add import command to justfile
    
    * Add regular run alias to justfile
    
    * Fix reference issue with upload script if officers aren't defined
    
    * Various fixes, add running logic
    
    * Add logging, save off links that don't have badge numbers in OO

---
+++ data-munging/divest_spd_links.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+import click
+import pandas as pd
+
+
+log = logging.getLogger()
+
+
+def main(id_path: Path, link_path: Path, output: Path):
+    log.info("Starting import")
+    ids = pd.read_csv(id_path, usecols=["id", "badge number"])
+    links = pd.read_csv(link_path)
+    # Collapse the multiple links columns into a single column list
+    links["url"] = links[
+        ["Links", "last updated: 8/6/2021", "Unnamed: 6"]
+    ].values.tolist()
+    # Make the badge column a string
+    links["badge number"] = links["Badge Number"].astype(str)
+    # Subset the columns at this point
+    links = links[["badge number", "url"]]
+    # Explode the list of links into individual rows
+    links = links.explode("url", ignore_index=True)
+    # Drop any rows where a link doesn't exist (this will be most)
+    links = links[links["url"].notna()]
+    # Join the two dataframes on badge number
+    merged = links.merge(ids, how="left").astype({"id": pd.Int64Dtype()})
+    # Split off the links that don't have an OpenOversight badge associated with them
+    _has_id = merged["id"].notna()
+    missing = merged[~_has_id]
+    # Remove those missing urls
+    merged = merged[_has_id][["id", "url"]]
+    # Rename id to "officer_ids", used in importer:
+    # https://openoversight.readthedocs.io/en/latest/advanced_csv_import.html#links-csv
+    merged.columns = ["officer_ids", "url"]
+    # Add the extra column info
+    merged["title"] = "Divest SPD Twitter thread"
+    merged["link_type"] = "Link"
+    merged["author"] = "Divest SPD"
+    # Modify the information for tweets not made by Divest SPD
+    _not_divest = ~merged["url"].str.contains("DivestSPD")
+    merged.loc[_not_divest, "title"] = "Meet SPD Twitter thread"
+    merged.loc[_not_divest, "author"] = "nbd1232"
+    # Add an empty id column
+    merged["id"] = None
+    missing_output = output.parent / f"{output.stem}__missing.csv"
+    log.info(f"Writing {len(missing)} missing records to {missing_output}")
+    missing.to_csv(missing_output, index=False)
+    log.info(f"Writing {len(merged)} output records to {output}")
+    merged.to_csv(output, index=False)
+    log.info("Finished")
+
+
+@click.command()
+@click.argument("id_path", type=click.Path(exists=True, path_type=Path))
+@click.argument("link_path", type=click.Path(exists=True, path_type=Path))
+@click.argument("output", type=click.Path(exists=True, path_type=Path))
+def cli(id_path: Path, link_path: Path, output: Path):
+    logging.basicConfig(
+        format="[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s",
+        level=logging.INFO,
+    )
+    main(id_path, link_path, output)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ justfile
@@ -0,0 +1,31 @@
+DC := "docker-compose"
+RUN := DC + " run --rm web"
+set dotenv-load := false
+
+
+build:
+	{{ DC }} build
+
+up service="":
+	{{ DC }} up -d {{ service }}
+
+down:
+	{{ DC }} down
+
+fresh-start:
+	# Tear down existing containers, remove volume
+	{{ DC }} down
+	docker volume rm openoversight_postgres openoversight_minio || true
+	{{ DC }} build
+
+	# Start up and populate fields
+	{{ RUN }} python ../create_db.py
+	{{ RUN }} flask make-admin-user
+	{{ RUN }} flask add-department "Seattle Police Department" "SPD"
+	{{ RUN }} flask bulk-add-officers /data/init_data.csv
+
+run +args:
+	{{ RUN }} {{ args }}
+
+import +args:
+	{{ RUN }} flask advanced-csv-import {{ args }}
