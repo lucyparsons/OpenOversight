230c12548b6e1ea188d5347f629565d98a55c3a9
sea-kelp <66500457+sea-kelp@users.noreply.github.com>
2022-06-16 21:05:46 -0700

    Make rank/unit searchable (OrcaCollective#133)
    * Make rank/unit searchable
    
    * Update OpenOversight/app/templates/list_officer.html
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

---
+++ OpenOversight/app/static/js/list_officer.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    'use strict';
+
+    $(".select2-multi-search").select2({
+        "closeOnSelect": false,
+        "placeholder": "Select an option",
+        "width": "100%"
+    });
+});
