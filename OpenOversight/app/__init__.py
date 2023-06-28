10af5d70b42ee066766fbe32ebc75585342807cf
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-28 15:44:25 -0500

    Update __init__.py

--- OpenOversight/app/__init__.py
+++ OpenOversight/app/__init__.py
@@ -24,7 +24,7 @@ from .gmail_client import GmailClient
 
 bootstrap = Bootstrap()
 mail = Mail()
-gmail_client = GmailClient()
+GmailClient()
 
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
