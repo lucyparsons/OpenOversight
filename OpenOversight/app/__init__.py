09412a61a8a057ec1bf9e0498b1a10b10a4b2f57
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-29 14:38:04 -0500

    Remove flask_mail code

--- OpenOversight/app/__init__.py
+++ OpenOversight/app/__init__.py
@@ -12,7 +12,6 @@ from flask_bootstrap import Bootstrap
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sitemap import Sitemap
 from flask_wtf.csrf import CSRFProtect
@@ -23,7 +22,6 @@ from .gmail_client import GmailClient
 
 
 bootstrap = Bootstrap()
-mail = Mail()
 GmailClient()
 
 login_manager = LoginManager()
