06fc935a1b13605671207020ced9a150e94f437e
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-06 16:14:03 -0500

    name change

--- OpenOversight/app/csv_imports.py
+++ OpenOversight/app/csv_imports.py
@@ -4,7 +4,7 @@ from typing import Dict, Optional
 
 from sqlalchemy.exc import SQLAlchemyError
 
-from .model_imports import (
+from .database_imports import (
     create_assignment_from_dict,
     create_incident_from_dict,
     create_link_from_dict,
