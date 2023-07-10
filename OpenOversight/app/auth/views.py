777962028ee2c9ff6cb0796ba7814a5aa994cd20
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-10 10:42:04 -0500

    Update views.py

--- OpenOversight/app/auth/views.py
+++ OpenOversight/app/auth/views.py
@@ -11,6 +11,18 @@ from flask import (
 )
 from flask_login import current_user, login_required, login_user, logout_user
 
+from OpenOversight.app import sitemap
+from OpenOversight.app.auth import auth
+from OpenOversight.app.auth.forms import (
+    ChangeDefaultDepartmentForm,
+    ChangeEmailForm,
+    ChangePasswordForm,
+    EditUserForm,
+    LoginForm,
+    PasswordResetForm,
+    PasswordResetRequestForm,
+    RegistrationForm,
+)
 from OpenOversight.app.email_client import EmailClient
 from OpenOversight.app.models.database import User, db
 from OpenOversight.app.models.emails import (
@@ -23,19 +35,6 @@ from OpenOversight.app.models.emails import (
 from OpenOversight.app.utils.auth import admin_required
 from OpenOversight.app.utils.forms import set_dynamic_default
 from OpenOversight.app.utils.general import validate_redirect_url
-
-from .. import sitemap
-from . import auth
-from .forms import (
-    ChangeDefaultDepartmentForm,
-    ChangeEmailForm,
-    ChangePasswordForm,
-    EditUserForm,
-    LoginForm,
-    PasswordResetForm,
-    PasswordResetRequestForm,
-    RegistrationForm,
-)
 
 
 js_loads = ["js/zxcvbn.js", "js/password.js"]
