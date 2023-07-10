6d504ad59be0a264d660192c54e97398836de272
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-10 10:36:27 -0500

    Update utils.py

--- OpenOversight/app/auth/utils.py
+++ OpenOversight/app/auth/utils.py
@@ -1,29 +1,29 @@
 from functools import wraps
+from http import HTTPStatus
 
 from flask import abort
 from flask_login import current_user
 
 
-# TODO: Move these functions to the utils package
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if current_user.is_anonymous or not current_user.is_administrator:
-            abort(403)
+            abort(HTTPStatus.FORBIDDEN)
         return f(*args, **kwargs)
 
     return decorated_function
 
 
 def ac_or_admin_required(f):
-    """Decorate that requires that the user be an area coordinator or administrator"""
+    """Decorate that requires that the user be an area coordinator or administrator."""
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if current_user.is_anonymous or not (
             current_user.is_administrator or current_user.is_area_coordinator
         ):
-            abort(403)
+            abort(HTTPStatus.FORBIDDEN)
         return f(*args, **kwargs)
 
     return decorated_function
