f8277c13797faa02426f69e02fc9ced81d670cdf
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-18 11:03:59 -0500

    Update env.py

--- OpenOversight/migrations/env.py
+++ OpenOversight/migrations/env.py
@@ -50,7 +50,7 @@ def run_migrations_online():
     # this callback is used to prevent an auto-migration from being generated
     # when there are no changes to the schema
     # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
-    def process_revision_directives(directives):
+    def process_revision_directives(context, revision, directives):
         if getattr(config.cmd_opts, "autogenerate", False):
             script = directives[0]
             if script.upgrade_ops.is_empty():
