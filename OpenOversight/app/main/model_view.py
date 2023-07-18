c2bd06d168dc5c62417837118346a6ae7fc0ea27
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-18 16:07:26 -0500

    Update model_view.py

--- OpenOversight/app/main/model_view.py
+++ OpenOversight/app/main/model_view.py
@@ -166,8 +166,8 @@ class ModelView(MethodView):
 
     def populate_obj(self, form, obj):
         form.populate_obj(obj)
-        if hasattr(obj, "date_updated"):
-            obj.date_updated = datetime.datetime.now()
+        if hasattr(obj, "updated"):
+            obj.updated = datetime.datetime.now()
         db.session.add(obj)
         db.session.commit()
 
