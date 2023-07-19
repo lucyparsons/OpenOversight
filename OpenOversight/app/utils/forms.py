cf2bdb53abf74df462a51ae814a4d40a1d3b3d3f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-19 10:44:18 -0500

    Update forms.py

--- OpenOversight/app/utils/forms.py
+++ OpenOversight/app/utils/forms.py
@@ -89,8 +89,8 @@ def add_officer_profile(form, current_user):
                     note=note["text_contents"],
                     user_id=current_user.get_id(),
                     officer=officer,
-                    created=datetime.datetime.now(),
-                    updated=datetime.datetime.now(),
+                    created_at=datetime.datetime.now(),
+                    updated_at=datetime.datetime.now(),
                 )
                 db.session.add(new_note)
     if form.descriptions.data:
@@ -101,8 +101,8 @@ def add_officer_profile(form, current_user):
                     description=description["text_contents"],
                     user_id=current_user.get_id(),
                     officer=officer,
-                    created=datetime.datetime.now(),
-                    updated=datetime.datetime.now(),
+                    created_at=datetime.datetime.now(),
+                    updated_at=datetime.datetime.now(),
                 )
                 db.session.add(new_description)
     if form.salaries.data:
@@ -127,8 +127,8 @@ def create_description(self, form):
         text_contents=form.text_contents.data,
         creator_id=form.creator_id.data,
         officer_id=form.officer_id.data,
-        created=datetime.datetime.now(),
-        updated=datetime.datetime.now(),
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now(),
     )
 
 
@@ -190,8 +190,8 @@ def create_note(self, form):
         text_contents=form.text_contents.data,
         creator_id=form.creator_id.data,
         officer_id=form.officer_id.data,
-        created=datetime.datetime.now(),
-        updated=datetime.datetime.now(),
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now(),
     )
 
 
