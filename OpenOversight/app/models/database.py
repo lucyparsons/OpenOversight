aae3f36a241a62e359931f4f97e459ca2496cb67
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-25 14:09:41 -0500

    Update database.py

--- OpenOversight/app/models/database.py
+++ OpenOversight/app/models/database.py
@@ -39,9 +39,9 @@ officer_incidents = db.Table(
 class Department(BaseModel):
     __tablename__ = "departments"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), index=True, unique=True, nullable=False)
+    name = db.Column(db.String(255), index=False, unique=False, nullable=False)
     short_name = db.Column(db.String(100), unique=False, nullable=False)
-    state = db.Column(db.String(2), index=True)
+    state = db.Column(db.String(2), index=False, default="")
 
     # See https://github.com/lucyparsons/OpenOversight/issues/462
     unique_internal_identifier_label = db.Column(
