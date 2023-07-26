ecfe5260ef6726e2a0cafd7392b186e6d4c0ab1b
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-26 00:35:50 -0500

    Update test_utils.py

--- OpenOversight/tests/test_utils.py
+++ OpenOversight/tests/test_utils.py
@@ -31,12 +31,14 @@ upload_s3_patch = patch(
 class PoliceDepartment:
     """Base Police Department class."""
 
-    def __init__(self, name, short_name, state=random.choice(us.STATES).abbr):
+    def __init__(self, name, short_name, state="", unique_internal_identifier_label=""):
         self.name = name
         self.short_name = short_name
-        self.state = state
-        self.unique_internal_identifier_label = "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=20)
+        self.state = state if state else random.choice(us.STATES).abbr
+        self.unique_internal_identifier_label = (
+            unique_internal_identifier_label
+            if unique_internal_identifier_label
+            else "".join(random.choices(string.ascii_uppercase + string.digits, k=20))
         )
 
 
