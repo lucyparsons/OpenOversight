d8b8b6f3e804f7092a31a44e67b3040ae61f3714
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-27 15:39:09 -0500

    Update 2023-07-27-1848_9fa948bcea25_change_incident_date_and_time_to_.py

--- OpenOversight/migrations/versions/2023-07-27-1848_9fa948bcea25_change_incident_date_and_time_to_.py
+++ OpenOversight/migrations/versions/2023-07-27-1848_9fa948bcea25_change_incident_date_and_time_to_.py
@@ -7,13 +7,14 @@ Create Date: 2023-07-27 18:48:58.819477
 """
 import os
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
+
 # revision identifiers, used by Alembic.
-revision = '9fa948bcea25'
-down_revision = '18f43ac4622f'
+revision = "9fa948bcea25"
+down_revision = "18f43ac4622f"
 branch_labels = None
 depends_on = None
 
@@ -21,31 +22,34 @@ TIMEZONE = os.getenv("TIMEZONE", "America/Chicago")
 
 
 def upgrade():
-    op.add_column('incidents', sa.Column('occurred_at', sa.DateTime(timezone=True), nullable=True))
+    op.add_column(
+        "incidents", sa.Column("occurred_at", sa.DateTime(timezone=True), nullable=True)
+    )
     op.execute(
         f"""
             UPDATE incidents
-            SET occurred_at = (date::date || ' ' || COALESCE(time, 
+            SET occurred_at = (date::date || ' ' || COALESCE(time,
                 '00:00:00')::timetz)::timestamp AT TIME ZONE '{TIMEZONE}'
             WHERE occurred_at IS NULL
         """
     )
-    op.alter_column('incidents', 'occurred_at', nullable=False)
-    op.drop_index('ix_incidents_date')
-    op.drop_index('ix_incidents_time')
-    op.drop_column('incidents', 'time')
-    op.drop_column('incidents', 'date')
+    op.alter_column("incidents", "occurred_at", nullable=False)
+    op.drop_index("ix_incidents_date")
+    op.drop_index("ix_incidents_time")
+    op.drop_column("incidents", "time")
+    op.drop_column("incidents", "date")
 
 
 def downgrade():
-    op.add_column('incidents', sa.Column('date', sa.DATE(), nullable=True))
-    op.add_column('incidents', sa.Column('time', postgresql.TIME(), nullable=True))
+    op.add_column("incidents", sa.Column("date", sa.DATE(), nullable=True))
+    op.add_column("incidents", sa.Column("time", postgresql.TIME(), nullable=True))
     op.execute(
         f"""
         UPDATE incidents
         SET (date, time) = (occurred_at::date, (occurred_at::timestamptz AT TIME ZONE '{TIMEZONE}')::time)
-        """)
+        """
+    )
 
-    op.create_index(op.f('ix_incidents_time'), 'incidents', ['time'], unique=False)
-    op.create_index(op.f('ix_incidents_date'), 'incidents', ['date'], unique=False)
-    op.drop_column('incidents', 'occurred_at')
+    op.create_index(op.f("ix_incidents_time"), "incidents", ["time"], unique=False)
+    op.create_index(op.f("ix_incidents_date"), "incidents", ["date"], unique=False)
+    op.drop_column("incidents", "occurred_at")
