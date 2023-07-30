ae10f2c3d43d3a0377b4d99b05522f2b776b38e1
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-30 00:52:14 -0500

    Update test_functional.py

--- OpenOversight/tests/test_functional.py
+++ OpenOversight/tests/test_functional.py
@@ -12,6 +12,7 @@ from sqlalchemy.sql.expression import func
 
 from OpenOversight.app.models.database import Department, Incident, Officer, Unit, db
 from OpenOversight.app.utils.constants import KEY_OFFICERS_PER_PAGE
+from OpenOversight.tests.conftest import SPRINGFIELD_PD
 
 
 DESCRIPTION_CUTOFF = 700
@@ -140,9 +141,7 @@ def test_find_officer_can_see_uii_question_for_depts_with_uiis(
 ):
     browser.get(f"http://localhost:{server_port}/find")
 
-    dept_with_uii = Department.query.filter(
-        Department.unique_internal_identifier_label.isnot(None)
-    ).first()
+    dept_with_uii = Department.query.filter(id=SPRINGFIELD_PD.id).first()
     dept_id = str(dept_with_uii.id)
 
     dept_selector = Select(browser.find_element_by_id("dept"))
