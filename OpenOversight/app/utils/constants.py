7a3706b7f5e0cf92ffb05799e501097b389fa14f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-29 22:05:59 -0500

    Update constants.py

--- OpenOversight/app/utils/constants.py
+++ OpenOversight/app/utils/constants.py
@@ -1,6 +1,15 @@
 import os
 
 
+# Cache Key Constants
+KEY_TOTAL_ASSIGNMENTS = "total_assignments"
+KEY_TOTAL_INCIDENTS = "total_incidents"
+KEY_TOTAL_OFFICERS = "total_officers"
+
+# Config Key Constants
+KEY_OFFICERS_PER_PAGE = "OFFICERS_PER_PAGE"
+KEY_TIMEZONE = "TIMEZONE"
+
 # File Handling Constants
 ENCODING_UTF_8 = "utf-8"
 SAVED_UMASK = os.umask(0o077)  # Ensure the file is read/write by the creator only
@@ -8,11 +17,9 @@ SAVED_UMASK = os.umask(0o077)  # Ensure the file is read/write by the creator on
 # File Name Constants
 SERVICE_ACCOUNT_FILE = "service_account_key.json"
 
-# Key Constants
-KEY_OFFICERS_PER_PAGE = "OFFICERS_PER_PAGE"
-KEY_TIMEZONE = "TIMEZONE"
-
 # Numerical Constants
 BYTE = 1
 KILOBYTE = 1024 * BYTE
 MEGABYTE = 1024 * KILOBYTE
+MINUTE = 60
+HOUR = 60 * MINUTE
