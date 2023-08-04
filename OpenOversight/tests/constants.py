a107d389f6cb0afec2887303006e990dbd8aeeeb
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-03 21:30:51 -0500

    Create constants.py

---
+++ OpenOversight/tests/constants.py
@@ -0,0 +1,11 @@
+# User Constants
+AC_USER_EMAIL = "raq929@example.org"
+AC_USER_PASSWORD = "horse"
+ADMIN_USER_EMAIL = "test@example.org"
+ADMIN_USER_PASSWORD = "testtest"
+DISABLED_USER_EMAIL = "may@example.org"
+DISABLED_USER_PASSWORD = "yam"
+MOD_DISABLED_USER_EMAIL = "sam@example.org"
+MOD_DISABLED_USER_PASSWORD = "the yam"
+UNCONFIRMED_USER_EMAIL = "freddy@example.org"
+UNCONFIRMED_USER_PASSWORD = "dog"
