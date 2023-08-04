4660f7897e3d16196843d6da89cd768189014051
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-04 15:19:52 -0500

    Update cache_client.py

--- OpenOversight/app/cache_client.py
+++ OpenOversight/app/cache_client.py
@@ -1,21 +1,25 @@
-from cachetools import TTLCache, cached
+from cachetools import TTLCache
 from cachetools.keys import hashkey
 
 from OpenOversight.app.utils.constants import HOUR
 
 
-class CacheClient(object):
+DB_CACHE = TTLCache(maxsize=1024, ttl=12 * HOUR)
+
+
+def department_statistics_cache_key(update_type: str):
+    """Return a key function to calculate the cache key for Department
+    methods using the department id and a given update type.
+
+    Department.id is used instead of a Department obj because the default Python
+    __hash__ is unique per obj instance, meaning multiple instances of the same
+    department will have different hashes.
+
+    Update type is used in the hash to differentiate between the update types we compute
+    per department.
     """
-    CacheClient is a Singleton class that is used for the TTLCache client.
-    This can be fairly easily switched out with another cache type, but it is
-    currently defaulted to TTLCache.
-    """
 
-    CACHE_DURATION = 12 * HOUR
+    def _cache_key(department):
+        return hashkey(department.id, update_type)
 
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = TTLCache(maxsize=1024, ttl=cls.CACHE_DURATION)
-        return cls._instance
+    return _cache_key
