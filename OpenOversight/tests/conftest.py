fc62895673edb50b0c892a5dd43163d85773dc83
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-08 12:13:12 -0500

    Update conftest.py

--- OpenOversight/tests/conftest.py
+++ OpenOversight/tests/conftest.py
@@ -213,7 +213,7 @@ def build_assignment(
     return Assignment(
         star_no=pick_star(),
         job_id=random.choice(jobs).id,
-        officer=officer,
+        base_officer=officer,
         unit_id=unit_id,
         start_date=pick_date(officer.full_name().encode(ENCODING_UTF_8)),
         resign_date=pick_date(officer.full_name().encode(ENCODING_UTF_8)),
