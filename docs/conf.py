0dd596c7ccc7a86554674c84f72677e704d05d97
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-12 01:18:31 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -129,21 +129,3 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [(master_doc, "openoversight", "OpenOversight Documentation", [author], 1)]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "OpenOversight",
-        "OpenOversight Documentation",
-        author,
-        "OpenOversight",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
