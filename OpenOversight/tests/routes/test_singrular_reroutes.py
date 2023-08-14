30bc4832eeed459fdf8d9ced3a38aa8b98e498e6
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-14 16:53:56 -0500

    Create test_singrular_reroutes.py

---
+++ OpenOversight/tests/routes/test_singrular_reroutes.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from flask import current_app, url_for
+
+from OpenOversight.app.utils.constants import FLASH_MSG_PERMANENT_REDIRECT
+
+
+def test_redirect_get_started_labeling(client, session):
+    with current_app.test_request_context():
+        resp_no_redirect = client.get(
+            url_for("main.redirect_get_started_labeling"), follow_redirects=False
+        )
+        with client.session_transaction() as session:
+            flash_message = dict(session['_flashes']).get('message')
+
+        assert resp_no_redirect.status_code == HTTPStatus.PERMANENT_REDIRECT
+        assert flash_message == FLASH_MSG_PERMANENT_REDIRECT
+
+        resp_redirect = client.get(
+            url_for("main.redirect_get_started_labeling"), follow_redirects=True
+        )
+        assert resp_redirect.status_code == HTTPStatus.OK
+        assert resp_redirect.request.path == url_for("main.get_started_labeling")
