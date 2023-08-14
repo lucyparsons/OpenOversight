f5ee01e03d48ec8555e48902469964e13bf6e5b8
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-14 17:00:57 -0500

    Update test_singrular_reroutes.py

--- OpenOversight/tests/routes/test_singrular_reroutes.py
+++ OpenOversight/tests/routes/test_singrular_reroutes.py
@@ -11,7 +11,7 @@ def test_redirect_get_started_labeling(client, session):
             url_for("main.redirect_get_started_labeling"), follow_redirects=False
         )
         with client.session_transaction() as session:
-            flash_message = dict(session['_flashes']).get('message')
+            flash_message = dict(session["_flashes"]).get("message")
 
         assert resp_no_redirect.status_code == HTTPStatus.PERMANENT_REDIRECT
         assert flash_message == FLASH_MSG_PERMANENT_REDIRECT
@@ -21,3 +21,26 @@ def test_redirect_get_started_labeling(client, session):
         )
         assert resp_redirect.status_code == HTTPStatus.OK
         assert resp_redirect.request.path == url_for("main.get_started_labeling")
+
+
+def test_redirect_officer_profile(client, session):
+    with current_app.test_request_context():
+        officer_id = 1
+        resp_no_redirect = client.get(
+            url_for("main.redirect_officer_profile", officer_id=officer_id),
+            follow_redirects=False,
+        )
+        with client.session_transaction() as session:
+            flash_message = dict(session["_flashes"]).get("message")
+
+        assert resp_no_redirect.status_code == HTTPStatus.PERMANENT_REDIRECT
+        assert flash_message == FLASH_MSG_PERMANENT_REDIRECT
+
+        resp_redirect = client.get(
+            url_for("main.redirect_officer_profile", officer_id=officer_id),
+            follow_redirects=True,
+        )
+        assert resp_redirect.status_code == HTTPStatus.OK
+        assert resp_redirect.request.path == url_for(
+            "main.officer_profile", officer_id=officer_id
+        )
