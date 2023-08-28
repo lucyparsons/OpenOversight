ef103bf502b9839714948f5cfe97adae38f5d8c9
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-08-28 16:46:10 -0500

    Add image carousel for officers with multiple images (#1046)
    ## Fixes issue
    https://github.com/lucyparsons/OpenOversight/issues/1045
    
    ## Description of Changes
    Added image carousel for officers with multiple images and additional
    test coverage.
    
    ## Screenshots (if appropriate)
    Multiple images:
    <img width="1251" alt="Screenshot 2023-08-27 at 1 27 07 AM"
    src="https://github.com/lucyparsons/OpenOversight/assets/5885605/e7858594-557d-4e8f-9c08-bb92c854ec8a">
    
    Single image:
    <img width="1253" alt="Screenshot 2023-08-27 at 1 27 37 AM"
    src="https://github.com/lucyparsons/OpenOversight/assets/5885605/f46a19aa-326a-4e13-9561-d22b345ee2aa">
    
    No images:
    <img width="1251" alt="Screenshot 2023-08-27 at 1 27 58 AM"
    src="https://github.com/lucyparsons/OpenOversight/assets/5885605/b3f3c37a-7542-422d-9e54-b788e01dec4a">
    
    ## Tests and linting
     - [x] This branch is up-to-date with the `develop` branch.
     - [x] `pytest` passes on my local development environment.
     - [x] `pre-commit` passes on my local development environment.

--- OpenOversight/app/models/database.py
+++ OpenOversight/app/models/database.py
@@ -554,7 +554,12 @@ class Location(BaseModel, TrackUpdates):
         return state_validator(state)
 
     def __repr__(self):
-        if self.street_name and self.cross_street2:
+        if self.street_name and self.cross_street1 and self.cross_street2:
+            return (
+                f"Intersection of {self.street_name} between {self.cross_street1} "
+                f"and {self.cross_street2}, {self.city} {self.state}"
+            )
+        elif self.street_name and self.cross_street2:
             return (
                 f"Intersection of {self.street_name} and {self.cross_street2}, "
                 + f"{self.city} {self.state}"
@@ -563,11 +568,6 @@ class Location(BaseModel, TrackUpdates):
             return (
                 f"Intersection of {self.street_name} and {self.cross_street1}, "
                 + f"{self.city} {self.state}"
-            )
-        elif self.street_name and self.cross_street1 and self.cross_street2:
-            return (
-                f"Intersection of {self.street_name} between {self.cross_street1} "
-                f"and {self.cross_street2}, {self.city} {self.state}"
             )
         else:
             return f"{self.city} {self.state}"
