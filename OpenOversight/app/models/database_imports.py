4c318b1dd1e8efbb167d5cdb791fc12346b28a89
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-09 20:50:29 -0500

    Update database_imports.py

--- OpenOversight/app/models/database_imports.py
+++ OpenOversight/app/models/database_imports.py
@@ -118,6 +118,7 @@ def update_officer_from_dict(data: Dict[str, Any], officer: Officer) -> Officer:
         officer.unique_internal_identifier = parse_str(
             data.get("unique_internal_identifier"), None
         )
+    officer.last_updated_at = datetime.now()
     db.session.flush()
     return officer
 
@@ -155,6 +156,7 @@ def update_assignment_from_dict(
         assignment.start_date = parse_date(data.get("start_date"))
     if "resign_date" in data.keys():
         assignment.resign_date = parse_date(data.get("resign_date"))
+    assignment.last_updated_at = datetime.now()
     db.session.flush()
 
     return assignment
@@ -186,6 +188,7 @@ def update_salary_from_dict(data: Dict[str, Any], salary: Salary) -> Salary:
         salary.year = int(data["year"])
     if "is_fiscal_year" in data.keys():
         salary.is_fiscal_year = parse_bool(data.get("is_fiscal_year"))
+    salary.last_updated_at = datetime.now()
     db.session.flush()
 
     return salary
@@ -230,6 +233,7 @@ def update_link_from_dict(data: Dict[str, Any], link: Link) -> Link:
         link.officers = data.get("officers") or []
     if "incidents" in data:
         link.incidents = data.get("incidents") or []
+    link.last_updated_at = datetime.now()
     db.session.flush()
 
     return link
@@ -315,11 +319,11 @@ def update_incident_from_dict(data: Dict[str, Any], incident: Incident) -> Incid
         incident.created_by = parse_int(data.get("created_by"))
     if "last_updated_by" in data:
         incident.last_updated_by = parse_int(data.get("last_updated_by"))
-        incident.last_updated_at = datetime.now()
     if "officers" in data:
         incident.officers = data["officers"] or []
     if "license_plate_objects" in data:
         incident.license_plates = data["license_plate_objects"] or []
+    incident.last_updated_at = datetime.now()
     db.session.flush()
     return incident
 
