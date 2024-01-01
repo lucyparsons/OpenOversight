29783c9994820b3efd6c8c01918ee088ffc9a217
Spraynard <priv4te3y3@protonmail.com>
2024-01-01 02:32:45 -0500

    Modifying forms.py
    Adding default of "0" to overtime_pay field

--- OpenOversight/app/main/forms.py
+++ OpenOversight/app/main/forms.py
@@ -171,7 +171,9 @@ class SalaryForm(Form):
         "Salary", validators=[NumberRange(min=0, max=1000000), validate_money]
     )
     overtime_pay = DecimalField(
-        "Overtime Pay", validators=[NumberRange(min=0, max=1000000), validate_money]
+        "Overtime Pay", 
+        default=0,
+        validators=[NumberRange(min=0, max=1000000), validate_money]
     )
     year = IntegerField(
         "Year",
