lucyparsons/OpenOversight

commit 70c158325509a8268ee0c5ff65724301a6771c3a

Pluralize REST routes (#1018)
## Fixes issue
https://github.com/lucyparsons/OpenOversight/issues/647

## Description of Changes
I pluralized nouns in routes for the site, added tests for all
pluralized routes, fixed bugs that were not found, and added HTML files
that were missing.

<img width="909" alt="Screenshot 2023-08-10 at 11 12 25 AM"
src="https://github.com/lucyparsons/OpenOversight/assets/5885605/3f0e1448-6b46-4f8c-8419-52fa62157dce">

Source:
[Link](https://blog.dreamfactory.com/best-practices-for-naming-rest-api-endpoints/#:~:text=Using%20plural%20nouns%20is%20a,only%20represents%20a%20single%20user.)
Resource:
[Link](https://www.restapitutorial.com/lessons/restfulresourcenaming.html)

## Tests and linting
 - [x] This branch is up-to-date with the `develop` branch.
 - [x] `pytest` passes on my local development environment.
 - [x] `pre-commit` passes on my local development environment.

OpenOversight/app/templates/description_detail.html
@@ -1,0 +1,12 @@
+{% extends "base.html" %}
+{% block page_title %}
+  Description Details
+{% endblock page_title %}
+{% block form %}
+  <p>
+    For officer with OOID {{ form.officer_id.data }}.
+    <br>
+    {{ form.description }}
+  </p>
+  {{ super() }}
+{% endblock form %}

OpenOversight/app/templates/note_detail.html
@@ -1,0 +1,12 @@
+{% extends "base.html" %}
+{% block page_title %}
+  Note Details
+{% endblock page_title %}
+{% block form %}
+  <p>
+    For officer with OOID {{ form.officer_id.data }}.
+    <br>
+    {{ form.description }}
+  </p>
+  {{ super() }}
+{% endblock form %}

OpenOversight/tests/test_functional.py
@@ -97,7 +97,7 @@ def test_user_can_use_form_to_get_to_browse(mockdata, browser, server_port):
 
 def test_user_can_get_to_complaint(mockdata, browser, server_port):
     browser.get(
-        f"http://localhost:{server_port}/complaint?officer_star=6265&"
+        f"http://localhost:{server_port}/complaints?officer_star=6265&"
         "officer_first_name=IVANA&officer_last_name=SNOTBALL&officer_middle_initial="
         "&officer_image=static%2Fimages%2Ftest_cop2.png"
     )
@@ -114,7 +114,7 @@ def test_officer_browse_pagination(mockdata, browser, server_port):
     total = Officer.query.filter_by(department_id=AC_DEPT).count()
 
     # first page of results
-    browser.get(f"http://localhost:{server_port}/department/{AC_DEPT}?page=1")
+    browser.get(f"http://localhost:{server_port}/departments/{AC_DEPT}?page=1")
     wait_for_element(browser, By.TAG_NAME, "body")
     page_text = browser.find_element_by_tag_name("body").text
     expected = f"Showing 1-{current_app.config['OFFICERS_PER_PAGE']} of {total}"
@@ -123,7 +123,7 @@ def test_officer_browse_pagination(mockdata, browser, server_port):
     # last page of results
     last_page_index = (total // current_app.config[KEY_OFFICERS_PER_PAGE]) + 1
     browser.get(
-        f"http://localhost:{server_port}/department/{AC_DEPT}?page={last_page_index}"
+        f"http://localhost:{server_port}/departments/{AC_DEPT}?page={last_page_index}"
     )
     wait_for_element(browser, By.TAG_NAME, "body")
     page_text = browser.find_element_by_tag_name("body").text
@@ -176,7 +176,7 @@ def test_incident_detail_display_read_more_button_for_descriptions_over_cutoff(
     mockdata, browser, server_port
 ):
     # Navigate to profile page for officer with short and long incident descriptions
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
 
     incident_long_description = Incident.query.filter(
         func.length(Incident.description) > DESCRIPTION_CUTOFF
@@ -191,7 +191,7 @@ def test_incident_detail_truncate_description_for_descriptions_over_cutoff(
     mockdata, browser, server_port
 ):
     # Navigate to profile page for officer with short and long incident descriptions
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
 
     incident_long_description = Incident.query.filter(
         func.length(Incident.description) > DESCRIPTION_CUTOFF
@@ -211,7 +211,7 @@ def test_incident_detail_do_not_display_read_more_button_for_descriptions_under_
     mockdata, browser, server_port
 ):
     # Navigate to profile page for officer with short and long incident descriptions
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
 
     # Select incident for officer that has description under cutoff chars
     result = browser.find_element_by_id("description-overflow-row_1")
@@ -220,7 +220,7 @@ def test_incident_detail_do_not_display_read_more_button_for_descriptions_under_
 
 def test_click_to_read_more_displays_full_description(mockdata, browser, server_port):
     # Navigate to profile page for officer with short and long incident descriptions
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
 
     incident_long_description = Incident.query.filter(
         func.length(Incident.description) > DESCRIPTION_CUTOFF
@@ -240,7 +240,7 @@ def test_click_to_read_more_displays_full_description(mockdata, browser, server_
 
 def test_click_to_read_more_hides_the_read_more_button(mockdata, browser, server_port):
     # Navigate to profile page for officer with short and long incident descriptions
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
 
     incident_long_description = Incident.query.filter(
         func.length(Incident.description) > DESCRIPTION_CUTOFF
@@ -268,13 +268,13 @@ def test_officer_form_has_units_alpha_sorted(mockdata, browser, server_port):
     db_units_sorted.insert(0, "None")
 
     # Check for the Unit sort on the 'add officer' form
-    browser.get(f"http://localhost:{server_port}/officer/new")
+    browser.get(f"http://localhost:{server_port}/officers/new")
     unit_select = Select(browser.find_element_by_id("unit"))
     select_units_sorted = list(map(lambda x: x.text, unit_select.options))
     assert db_units_sorted == select_units_sorted
 
     # Check for the Unit sort on the 'add assignment' form
-    browser.get(f"http://localhost:{server_port}/officer/1")
+    browser.get(f"http://localhost:{server_port}/officers/1")
     unit_select = Select(browser.find_element_by_id("unit"))
     select_units_sorted = list(map(lambda x: x.text, unit_select.options))
     assert db_units_sorted == select_units_sorted
@@ -292,7 +292,7 @@ def test_edit_officer_form_coerces_none_race_or_gender_to_not_sure(
     login_admin(browser, server_port)
 
     # Navigate to edit officer page for officer having NULL race and gender
-    browser.get(f"http://localhost:{server_port}/officer/1/edit")
+    browser.get(f"http://localhost:{server_port}/officers/1/edit")
 
     wait_for_element(browser, By.ID, "gender")
     select = Select(browser.find_element_by_id("gender"))
@@ -315,7 +315,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     login_admin(browser, server_port)
 
     # 1. Create new department (to avoid mockdata)
-    browser.get(f"http://localhost:{server_port}/department/new")
+    browser.get(f"http://localhost:{server_port}/departments/new")
     wait_for_page_load(browser)
     browser.find_element(By.ID, "name").send_keys("Auburn Police Department")
     browser.find_element(By.ID, "short_name").send_keys("APD")
@@ -323,7 +323,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     wait_for_page_load(browser)
 
     # 2. Add a new officer
-    browser.get(f"http://localhost:{server_port}/officer/new")
+    browser.get(f"http://localhost:{server_port}/officers/new")
     wait_for_page_load(browser)
 
     dept_select = Select(browser.find_element("id", "department"))
@@ -351,7 +351,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     wait_for_element(browser, By.CLASS_NAME, "dz-success")
 
     # 4. Classify the uploaded image
-    browser.get(f"http://localhost:{server_port}/sort/department/{dept_id}")
+    browser.get(f"http://localhost:{server_port}/sort/departments/{dept_id}")
 
     # Check that image loaded correctly: https://stackoverflow.com/a/36296478
     wait_for_element(browser, By.TAG_NAME, "img")
@@ -366,7 +366,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     assert "All images have been classified!" in page_text
 
     # 5. Identify the new officer in the uploaded image
-    browser.get(f"http://localhost:{server_port}/cop_face/department/{dept_id}")
+    browser.get(f"http://localhost:{server_port}/cop_faces/departments/{dept_id}")
     wait_for_page_load(browser)
     browser.find_element(By.ID, "officer_id").send_keys(officer_id)
     browser.find_element(By.CSS_SELECTOR, "input[value='Add identified face']").click()
@@ -380,7 +380,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     wait_for_page_load(browser)
 
     # 7. Check that the tag appears on the officer page
-    browser.get(f"http://localhost:{server_port}/officer/{officer_id}")
+    browser.get(f"http://localhost:{server_port}/officers/{officer_id}")
     wait_for_page_load(browser)
     browser.find_element(By.CSS_SELECTOR, "a > img.officer-face").click()
 

OpenOversight/app/auth/views.py
@@ -281,10 +281,7 @@ def change_dept():
 @auth.route("/users/", methods=[HTTPMethod.GET])
 @admin_required
 def get_users():
-    if request.args.get("page"):
-        page = int(request.args.get("page"))
-    else:
-        page = 1
+    page = int(request.args.get("page", 1))
     users = User.query.order_by(User.username).paginate(
         page=page, per_page=current_app.config["USERS_PER_PAGE"], error_out=False
     )

OpenOversight/app/templates/partials/officer_assignment_history.html
@@ -81,7 +81,7 @@
 </table>
 {% if is_admin_or_coordinator %}
   <h3>
-    Add Assignment<small>Admin only</small>
+    Add Assignment <small>Admin only</small>
   </h3>
   <table class="table">
     <tbody>

OpenOversight/app/utils/constants.py
@@ -23,6 +23,11 @@ KEY_OFFICERS_PER_PAGE = "OFFICERS_PER_PAGE"
 KEY_OO_MAIL_SUBJECT_PREFIX = "OO_MAIL_SUBJECT_PREFIX"
 KEY_TIMEZONE = "TIMEZONE"
 
+# Flash Message Constants
+FLASH_MSG_PERMANENT_REDIRECT = (
+    "This page's address has changed, please update your bookmark!"
+)
+
 # File Handling Constants
 ENCODING_UTF_8 = "utf-8"
 SAVED_UMASK = os.umask(0o077)  # Ensure the file is read/write by the creator only

OpenOversight/tests/routes/test_descriptions.py
@@ -14,9 +14,9 @@ from OpenOversight.tests.routes.route_helpers import login_ac, login_admin, logi
 @pytest.mark.parametrize(
     "route",
     [
-        "officer/1/description/1/edit",
-        "officer/1/description/new",
-        "officer/1/description/1/delete",
+        "officers/1/descriptions/1/edit",
+        "officers/1/descriptions/new",
+        "officers/1/descriptions/1/delete",
     ],
 )
 def test_route_login_required(route, client, mockdata):
@@ -27,9 +27,9 @@ def test_route_login_required(route, client, mockdata):
 @pytest.mark.parametrize(
     "route",
     [
-        "officer/1/description/1/edit",
-        "officer/1/description/new",
-        "officer/1/description/1/delete",
+        "officers/1/descriptions/1/edit",
+        "officers/1/descriptions/new",
+        "officers/1/descriptions/1/delete",
     ],
 )
 def test_route_admin_or_required(route, client, mockdata):

commit 429869c9f093e1f5b73f4755f9fa2fe8a7d71c18

Fix upload bug (#1095)
## Description of Changes
*Cherry-pick of
https://github.com/OrcaCollective/OpenOversight/pull/434*

* Fixed bug introduced in
https://github.com/lucyparsons/OpenOversight/pull/1018 where anonymous
uploads were silently failing due to misplaced `@login_required`
decorator
* Audited all other redirect routes
* Fixed note_detail.html and description_detail.html

## Notes for Deployment
None!

## Screenshots (if appropriate)
N/A

## Tests and Linting
 - [x] This branch is up-to-date with the `develop` branch.
 - [x] `pytest` passes on my local development environment.
 - [x] `pre-commit` passes on my local development environment.

OpenOversight/app/main/views.py
@@ -366,10 +366,7 @@ def sitemap_officers():
         yield "main.officer_profile", {"officer_id": officer.id}
 
 
-@main.route(
-    "/officer/<int:officer_id>/assignment/new",
-    methods=[HTTPMethod.GET, HTTPMethod.POST],
-)
+@main.route("/officer/<int:officer_id>/assignment/new", methods=[HTTPMethod.POST])
 @ac_or_admin_required
 def redirect_add_assignment(officer_id: int):
     return redirect(
@@ -1059,7 +1056,7 @@ def list_officer(
 
 
 @main.route("/department/<int:department_id>/ranks")
-def redirect_get_dept_ranks(department_id: int = 0, is_sworn_officer: bool = False):
+def redirect_get_dept_ranks(department_id: int, is_sworn_officer: bool = False):
     flash(FLASH_MSG_PERMANENT_REDIRECT)
     return redirect(
         url_for(
@@ -1073,7 +1070,7 @@ def redirect_get_dept_ranks(department_id: int = 0, is_sworn_officer: bool = Fal
 
 @main.route("/departments/<int:department_id>/ranks")
 @main.route("/ranks")
-def get_dept_ranks(department_id: int = 0, is_sworn_officer: bool = False):
+def get_dept_ranks(department_id: Optional[int] = None, is_sworn_officer: bool = False):
     if not department_id:
         department_id = request.args.get("department_id")
     if request.args.get("is_sworn_officer"):
@@ -1098,17 +1095,17 @@ def get_dept_ranks(department_id: int = 0, is_sworn_officer: bool = False):
 
 
 @main.route("/department/<int:department_id>/units")
-def redirect_get_dept_units(department_id: int = 0):
+def redirect_get_dept_units(department_id: int):
     flash(FLASH_MSG_PERMANENT_REDIRECT)
     return redirect(
-        url_for("main.get_dept_ranks", department_id=department_id),
+        url_for("main.get_dept_units", department_id=department_id),
         code=HTTPStatus.PERMANENT_REDIRECT,
     )
 
 
 @main.route("/departments/<int:department_id>/units")
 @main.route("/units")
-def get_dept_units(department_id: int = 0):
+def get_dept_units(department_id: Optional[int] = None):
     if not department_id:
         department_id = request.args.get("department_id")
 
@@ -1292,7 +1289,6 @@ def delete_tag(tag_id: int):
 
 @main.route("/tag/set_featured/<int:tag_id>", methods=[HTTPMethod.POST])
 @login_required
-@ac_or_admin_required
 def redirect_set_featured_tag(tag_id: int):
     flash(FLASH_MSG_PERMANENT_REDIRECT)
     return redirect(
@@ -1342,7 +1338,7 @@ def leaderboard():
 
 
 @main.route(
-    "/cop_face/department/<int:department_id>/images/<int:image_id>",
+    "/cop_face/department/<int:department_id>/image/<int:image_id>",
     methods=[HTTPMethod.GET, HTTPMethod.POST],
 )
 @main.route("/cop_face/image/<int:image_id>", methods=[HTTPMethod.GET, HTTPMethod.POST])
@@ -1352,7 +1348,9 @@ def leaderboard():
 )
 @main.route("/cop_face/", methods=[HTTPMethod.GET, HTTPMethod.POST])
 @login_required
-def redirect_label_data(department_id: int = 0, image_id: int = 0):
+def redirect_label_data(
+    department_id: Optional[int] = None, image_id: Optional[int] = None
+):
     flash(FLASH_MSG_PERMANENT_REDIRECT)
     return redirect(
         url_for("main.label_data", department_id=department_id, image_id=image_id),
@@ -1373,7 +1371,7 @@ def redirect_label_data(department_id: int = 0, image_id: int = 0):
 )
 @main.route("/cop_faces/", methods=[HTTPMethod.GET, HTTPMethod.POST])
 @login_required
-def label_data(department_id: int = 0, image_id: int = 0):
+def label_data(department_id: Optional[int] = None, image_id: Optional[int] = None):
     if department_id:
         department = Department.query.filter_by(id=department_id).one()
         if image_id:
@@ -1842,8 +1840,7 @@ def submit_officer_images(officer_id: int):
     "/upload/department/<int:department_id>/officer/<int:officer_id>",
     methods=[HTTPMethod.POST],
 )
-@login_required
-def redirect_upload(department_id: int = 0, officer_id: int = 0):
+def redirect_upload(department_id: int, officer_id: Optional[int] = None):
     return redirect(
         url_for("main.upload", department_id=department_id, officer_id=officer_id),
         code=HTTPStatus.PERMANENT_REDIRECT,
@@ -1855,9 +1852,8 @@ def redirect_upload(department_id: int = 0, officer_id: int = 0):
     "/upload/departments/<int:department_id>/officers/<int:officer_id>",
     methods=[HTTPMethod.POST],
 )
-@login_required
 @limiter.limit("250/minute")
-def upload(department_id: int = 0, officer_id: int = 0):
+def upload(department_id: int, officer_id: Optional[int] = None):
     if officer_id:
         officer = Officer.query.filter_by(id=officer_id).first()
         if not officer:

OpenOversight/app/models/users.py
@@ -4,5 +4,7 @@ from OpenOversight.app.models.database import Department
 
 
 class AnonymousUser(AnonymousUserMixin):
+    id = None
+
     def is_admin_or_coordinator(self, department: Department) -> bool:
         return False

OpenOversight/app/templates/description_detail.html
@@ -1,12 +1,11 @@
 {% extends "base.html" %}
-{% block page_title %}
-  Description Details
-{% endblock page_title %}
-{% block form %}
-  <p>
-    For officer with OOID {{ form.officer_id.data }}.
-    <br>
-    {{ form.description }}
-  </p>
-  {{ super() }}
-{% endblock form %}
+{% block content %}
+  <div class="container theme-showcase" role="main">
+    <div class="page-header">
+      <h1>Viewing Description {{ obj.id }} on Officer {{ obj.officer_id }}</h1>
+    </div>
+    <p class="lead">
+      <p>{{ obj.text_contents | markdown }}</p>
+    </p>
+  </div>
+{% endblock content %}

OpenOversight/app/templates/note_delete.html
@@ -8,7 +8,7 @@
     <p class="lead">
       Are you sure you want to delete this note?
       This cannot be undone.
-      <form action="{{ url_for('main.note_api_edit', obj_id=obj.id, officer_id=obj.officer_id) }}"
+      <form action="{{ url_for('main.note_api_delete', obj_id=obj.id, officer_id=obj.officer_id) }}"
             method="post">
         <input type="hidden" name="csrf_token" value="{{ csrf_token() }}" />
         <button class="btn btn-danger" type="submit">Delete</button>

OpenOversight/app/templates/note_detail.html
@@ -1,12 +1,11 @@
 {% extends "base.html" %}
-{% block page_title %}
-  Note Details
-{% endblock page_title %}
-{% block form %}
-  <p>
-    For officer with OOID {{ form.officer_id.data }}.
-    <br>
-    {{ form.description }}
-  </p>
-  {{ super() }}
-{% endblock form %}
+{% block content %}
+  <div class="container theme-showcase" role="main">
+    <div class="page-header">
+      <h1>Viewing Note {{ obj.id }} on Officer {{ obj.officer_id }}</h1>
+    </div>
+    <p class="lead">
+      <p>{{ obj.text_contents | markdown }}</p>
+    </p>
+  </div>
+{% endblock content %}

OpenOversight/tests/test_functional.py
@@ -40,6 +40,19 @@ def login_admin(browser, server_port):
             wait_for_element(browser, By.TAG_NAME, "body")
 
 
+def logout(browser, server_port):
+    browser.get(f"http://localhost:{server_port}/auth/logout")
+    wait_for_page_load(browser)
+
+
+def submit_image_to_dropzone(browser, img_path):
+    # Submit files in selenium: https://stackoverflow.com/a/61566075
+    wait_for_element(browser, By.CLASS_NAME, "dz-hidden-input")
+    upload = browser.find_element(By.CLASS_NAME, "dz-hidden-input")
+    upload.send_keys(img_path)
+    wait_for_element(browser, By.CLASS_NAME, "dz-success")
+
+
 def wait_for_element(browser, locator, text, timeout=10):
     try:
         element_present = expected_conditions.presence_of_element_located(
@@ -359,12 +372,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     wait_for_page_load(browser)
 
     Select(browser.find_element("id", "department")).select_by_value(dept_id)
-
-    # Submit files in selenium: https://stackoverflow.com/a/61566075
-    wait_for_element(browser, By.CLASS_NAME, "dz-hidden-input")
-    upload = browser.find_element(By.CLASS_NAME, "dz-hidden-input")
-    upload.send_keys(img_path)
-    wait_for_element(browser, By.CLASS_NAME, "dz-success")
+    submit_image_to_dropzone(browser, img_path)
 
     # 4. Classify the uploaded image
     browser.get(f"http://localhost:{server_port}/sort/departments/{dept_id}")
@@ -392,8 +400,7 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
     assert "Tag added to database" in page_text
 
     # 6. Log out as admin
-    browser.get(f"http://localhost:{server_port}/auth/logout")
-    wait_for_page_load(browser)
+    logout(browser, server_port)
 
     # 7. Check that the tag appears on the officer page
     browser.get(f"http://localhost:{server_port}/officers/{officer_id}")
@@ -417,3 +424,50 @@ def test_image_classification_and_tagging(mockdata, browser, server_port):
         >= frame.location["y"] + frame.size["height"]
     )
     assert image.location["y"] <= frame.location["y"]
+
+
+@pytest.mark.skip("Enable once real file upload in tests is supported.")
+def test_anonymous_user_can_upload_image(mockdata, browser, server_port):
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    img_path = os.path.join(test_dir, "images/200Cat.jpeg")
+
+    login_admin(browser, server_port)
+
+    # 1. Create new department as admin (to avoid mockdata)
+    browser.get(f"http://localhost:{server_port}/departments/new")
+    wait_for_page_load(browser)
+    browser.find_element(By.ID, "name").send_keys("Auburn Police Department")
+    browser.find_element(By.ID, "short_name").send_keys("APD")
+    Select(browser.find_element(By.ID, "state")).select_by_value("WA")
+    browser.find_element(By.ID, "submit").click()
+    wait_for_page_load(browser)
+
+    # 2. Log out
+    logout(browser, server_port)
+
+    # 3. Upload image
+    browser.get(f"http://localhost:{server_port}/submit")
+    wait_for_page_load(browser)
+
+    dept_select = Select(browser.find_element("id", "department"))
+    dept_select.select_by_visible_text("[WA] Auburn Police Department")
+    dept_id = dept_select.first_selected_option.get_attribute("value")
+
+    submit_image_to_dropzone(browser, img_path)
+
+    # 4. Login as admin again
+    login_admin(browser, server_port)
+
+    # 5. Check that there is 1 image to classify
+    browser.get(f"http://localhost:{server_port}/sort/departments/{dept_id}")
+    wait_for_page_load(browser)
+
+    page_text = browser.find_element(By.TAG_NAME, "body").text
+    assert "Do you see uniformed law enforcement officers in the photo?" in page_text
+
+    browser.find_element(By.ID, "answer-yes").click()
+    wait_for_page_load(browser)
+
+    # 6. All images tagged!
+    page_text = browser.find_element(By.TAG_NAME, "body").text
+    assert "All images have been classified!" in page_text