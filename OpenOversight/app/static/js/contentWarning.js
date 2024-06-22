598926a0e1c1e36be566c1f4a3cb34379f6da5bf
sea-kelp <66500457+sea-kelp@users.noreply.github.com>
2024-06-22 14:32:40 -0700

    Fix content warning styles

--- OpenOversight/app/static/js/contentWarning.js
+++ OpenOversight/app/static/js/contentWarning.js
@@ -2,7 +2,7 @@ function createOverlay(container) {
     const warningText = $(
         "<span><h3>Content Warning</h3><p>This video may be disturbing for some viewers</p></span>"
     )
-    const hide = $('<button type="button" class="btn btn-lg">Show video</button>')
+    const hide = $('<button type="button" class="btn btn-lg btn-light">Show video</button>')
     hide.click(() => overlay.css("display", "none"))
 
     const wrapper = $("<div>")
