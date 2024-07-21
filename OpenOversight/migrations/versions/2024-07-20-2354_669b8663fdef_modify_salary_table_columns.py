b99dbcfddddb64ca2227cc9adc85020b4fc7693d
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-07-20 19:03:56 -0500

    Update 2024-07-20-2354_669b8663fdef_modify_salary_table_columns.py

--- OpenOversight/migrations/versions/2024-07-20-2354_669b8663fdef_modify_salary_table_columns.py
+++ OpenOversight/migrations/versions/2024-07-20-2354_669b8663fdef_modify_salary_table_columns.py
@@ -5,41 +5,46 @@ Revises: 939ea0f2b26d
 Create Date: 2024-07-20 23:54:04.176103
 
 """
-from alembic import op
+
 import sqlalchemy as sa
+from alembic import op
+
 from OpenOversight.app.models.database import Currency
 
 
-
-revision = '669b8663fdef'
-down_revision = '939ea0f2b26d'
+revision = "669b8663fdef"
+down_revision = "939ea0f2b26d"
 
 
 def upgrade():
-    with op.batch_alter_table('salaries', schema=None) as batch_op:
-        batch_op.alter_column('salary',
-               existing_type=sa.NUMERIC(),
-               type_=Currency(),
-               existing_nullable=False)
-        batch_op.alter_column('overtime_pay',
-               existing_type=sa.NUMERIC(),
-               type_=Currency(),
-               existing_nullable=True)
+    with op.batch_alter_table("salaries", schema=None) as batch_op:
+        batch_op.alter_column(
+            "salary",
+            existing_type=sa.NUMERIC(),
+            type_=Currency(),
+            existing_nullable=False,
+        )
+        batch_op.alter_column(
+            "overtime_pay",
+            existing_type=sa.NUMERIC(),
+            type_=Currency(),
+            existing_nullable=True,
+        )
 
 
 def downgrade():
     op.execute(
         """
-            ALTER TABLE salaries 
-            ALTER COLUMN overtime_pay TYPE NUMERIC 
+            ALTER TABLE salaries
+            ALTER COLUMN overtime_pay TYPE NUMERIC
             USING overtime_pay::NUMERIC
         """
     )
 
     op.execute(
         """
-            ALTER TABLE salaries 
-            ALTER COLUMN salary TYPE NUMERIC 
+            ALTER TABLE salaries
+            ALTER COLUMN salary TYPE NUMERIC
             USING salary::NUMERIC
         """
     )
