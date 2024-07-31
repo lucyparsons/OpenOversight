7f7cbdf2528ac83a96f823f61370eea8e6f6e902
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-07-31 17:04:44 -0500

    Update test_user.py

--- OpenOversight/tests/routes/test_user.py
+++ OpenOversight/tests/routes/test_user.py
@@ -2,8 +2,36 @@ from http import HTTPStatus
 
 from flask import current_app
 
+from OpenOversight.app.models.database import User
 from OpenOversight.app.utils.constants import ENCODING_UTF_8
+from OpenOversight.tests.constants import GENERAL_USER_EMAIL
 from OpenOversight.tests.routes.route_helpers import login_user
+
+
+def test_user_cannot_see_profile_if_not_logged_in(mockdata, client, session):
+    with current_app.test_request_context():
+        user = User.query.filter_by(email=GENERAL_USER_EMAIL).first()
+        rv = client.get(f"/user/{user.username}")
+
+        # Assert that there is a redirect
+        assert rv.status_code == HTTPStatus.FOUND
+
+def test_user_profile_for_invalid_regex_username(mockdata, client, session):
+    with current_app.test_request_context():
+        login_user(client)
+        rv = client.get("/user/this_name_is_mad]]bogus")
+
+        # Assert page returns error
+        assert rv.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_user_profile_for_invalid_username(mockdata, client, session):
+    with current_app.test_request_context():
+        login_user(client)
+        rv = client.get("/user/this_name_is_mad_bogus")
+
+        # Assert page returns error
+        assert rv.status_code == HTTPStatus.NOT_FOUND
 
 
 def test_user_can_see_own_profile(mockdata, client, session):
