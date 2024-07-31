130dc69b35c2f96b566553979137a6e808bf76d8
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-07-31 16:16:06 -0500

    Update test_user_api.py

--- OpenOversight/tests/routes/test_user_api.py
+++ OpenOversight/tests/routes/test_user_api.py
@@ -33,12 +33,8 @@ def test_user_api_login_required(route, methods, client, mockdata):
 def test_user_cannot_access_user_api(route, methods, mockdata, client, session):
     with current_app.test_request_context():
         login_user(client)
-        if HTTPMethod.GET in methods:
-            rv = client.get(route)
-            assert rv.status_code == HTTPStatus.FORBIDDEN
-        if HTTPMethod.POST in methods:
-            rv = client.post(route)
-            assert rv.status_code == HTTPStatus.FORBIDDEN
+        rv = client.get(route)
+        assert rv.status_code == HTTPStatus.FORBIDDEN
 
 
 @pytest.mark.parametrize("route,methods", routes_methods)
