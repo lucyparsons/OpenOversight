0e0e2f793b20d462b2eb6fb23831b0ee73c65c71
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-10-09 15:04:36 -0500

    Create 2024-10-09-2000_99c50fc8d294_complete_audit_field_addition_to_users.py

---
+++ OpenOversight/migrations/versions/2024-10-09-2000_99c50fc8d294_complete_audit_field_addition_to_users.py
@@ -0,0 +1,52 @@
+"""complete audit field addition to users
+
+Revision ID: 99c50fc8d294
+Revises: bf254c0961ca
+Create Date: 2024-10-09 20:00:55.155377
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+
+revision = '99c50fc8d294'
+down_revision = 'bf254c0961ca'
+
+
+def upgrade():
+    with op.batch_alter_table('users', schema=None) as batch_op:
+        batch_op.drop_column('approved')
+        batch_op.drop_column('confirmed')
+        batch_op.drop_column('is_disabled')
+
+
+def downgrade():
+    with op.batch_alter_table('users', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('is_disabled', sa.BOOLEAN(), autoincrement=False, nullable=True))
+        batch_op.add_column(sa.Column('confirmed', sa.BOOLEAN(), autoincrement=False, nullable=True))
+        batch_op.add_column(sa.Column('approved', sa.BOOLEAN(), autoincrement=False, nullable=True))
+
+    op.execute(
+        """
+            UPDATE users
+            SET is_disabled =  TRUE
+            WHERE disabled_at IS NOT NULL
+        """
+    )
+
+    op.execute(
+        """
+            UPDATE users
+            SET approved =  TRUE
+            WHERE approved_at IS NOT NULL
+        """
+    )
+
+    op.execute(
+        """
+            UPDATE users
+            SET confirmed =  TRUE
+            WHERE confirmed_at IS NOT NULL
+        """
+    )
