5de0e1998b5018ea5d1d44f82b463f404f48d7b8
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-10-09 15:05:02 -0500

    Update 2024-10-09-2000_99c50fc8d294_complete_audit_field_addition_to_users.py

--- OpenOversight/migrations/versions/2024-10-09-2000_99c50fc8d294_complete_audit_field_addition_to_users.py
+++ OpenOversight/migrations/versions/2024-10-09-2000_99c50fc8d294_complete_audit_field_addition_to_users.py
@@ -5,27 +5,33 @@ Revises: bf254c0961ca
 Create Date: 2024-10-09 20:00:55.155377
 
 """
-from alembic import op
+
 import sqlalchemy as sa
+from alembic import op
 
 
-
-revision = '99c50fc8d294'
-down_revision = 'bf254c0961ca'
+revision = "99c50fc8d294"
+down_revision = "bf254c0961ca"
 
 
 def upgrade():
-    with op.batch_alter_table('users', schema=None) as batch_op:
-        batch_op.drop_column('approved')
-        batch_op.drop_column('confirmed')
-        batch_op.drop_column('is_disabled')
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.drop_column("approved")
+        batch_op.drop_column("confirmed")
+        batch_op.drop_column("is_disabled")
 
 
 def downgrade():
-    with op.batch_alter_table('users', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('is_disabled', sa.BOOLEAN(), autoincrement=False, nullable=True))
-        batch_op.add_column(sa.Column('confirmed', sa.BOOLEAN(), autoincrement=False, nullable=True))
-        batch_op.add_column(sa.Column('approved', sa.BOOLEAN(), autoincrement=False, nullable=True))
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column("is_disabled", sa.BOOLEAN(), autoincrement=False, nullable=True)
+        )
+        batch_op.add_column(
+            sa.Column("confirmed", sa.BOOLEAN(), autoincrement=False, nullable=True)
+        )
+        batch_op.add_column(
+            sa.Column("approved", sa.BOOLEAN(), autoincrement=False, nullable=True)
+        )
 
     op.execute(
         """
