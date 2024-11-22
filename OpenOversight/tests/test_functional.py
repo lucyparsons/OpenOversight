9618f700ed565b4cb231a8f6dd5c873dd76e176a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-11-22 14:51:01 -0600

    Update test_functional.py

--- OpenOversight/tests/test_functional.py
+++ OpenOversight/tests/test_functional.py
@@ -163,6 +163,7 @@ def test_find_officer_can_see_uii_question_for_depts_with_uiis(
     ).first()
 
     dept_selector = Select(browser.find_element_by_id("dept"))
+    scroll_to_element(browser, dept_selector)
     uii_element = browser.find_element_by_id("uii-question")
 
     dept_selector.select_by_value(str(dept_with_uii.id))
