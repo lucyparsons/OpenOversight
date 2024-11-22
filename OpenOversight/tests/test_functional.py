939b8d96b88d8e1c8b81ce4d9478a362bd6d9974
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-11-22 16:17:41 -0600

    Update test_functional.py

--- OpenOversight/tests/test_functional.py
+++ OpenOversight/tests/test_functional.py
@@ -162,8 +162,8 @@ def test_find_officer_can_see_uii_question_for_depts_with_uiis(
         Department.unique_internal_identifier_label.is_not(None)
     ).first()
 
-    dept_selector = Select(browser.find_element_by_id("dept"))
-    uii_element = browser.find_element("id", "uii-question")
+    dept_selector = Select(browser.find_element(By.ID, "dept"))
+    uii_element = browser.find_element(By.ID, "uii-question")
 
     dept_selector.select_by_value(str(dept_with_uii.id))
     assert uii_element.is_displayed()
