8785be780455b691b5f75b6e249b83082751d9ed
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-11-22 15:39:31 -0600

    Update conftest.py

--- OpenOversight/tests/conftest.py
+++ OpenOversight/tests/conftest.py
@@ -16,7 +16,8 @@ from faker import Faker
 from flask import current_app
 from PIL import Image as Pimage
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from sqlalchemy.orm import scoped_session, sessionmaker
 from xvfbwrapper import Xvfb
 
@@ -907,7 +908,11 @@ def browser(app, server_port):
     # start headless webdriver
     visual_display = Xvfb()
     visual_display.start()
-    driver = webdriver.Firefox(service=Service(log_path="/tmp/geckodriver.log"))
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(
+        options=options, service=Service(log_path="/tmp/geckodriver.log")
+    )
     # wait for browser to start up
     sleep(3)
     yield driver
