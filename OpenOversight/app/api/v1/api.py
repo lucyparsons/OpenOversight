2ca4be73fd1801be956bb55ea718c984503cd893
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-12-18 22:22:48 -0600

    Update api.py

--- OpenOversight/app/api/v1/api.py
+++ OpenOversight/app/api/v1/api.py
@@ -1,4 +1,29 @@
+from http import HTTPMethod
+
 from flask import Blueprint
+from sqlalchemy.orm import joinedload
+
+from OpenOversight.app.models.database import Assignment, Department, Officer, db
+from OpenOversight.app.models.database_cache import (
+    get_database_cache_entry,
+    put_database_cache_entry,
+)
+from OpenOversight.app.utils.constants import KEY_DEPT_ALL_OFFICERS
 
 
 v1 = Blueprint("v1", __name__, url_prefix="/v1")
+
+
+@v1.route("/departments/<int:department_id>/officers", methods=[HTTPMethod.GET])
+def download_dept_officers(department_id: int):
+    cache_params = (Department(id=department_id), KEY_DEPT_ALL_OFFICERS)
+    officers = get_database_cache_entry(*cache_params)
+    if officers is None:
+        officers = (
+            db.session.query(Officer)
+            .options(joinedload(Officer.assignments).joinedload(Assignment.job))
+            .options(joinedload(Officer.salaries))
+            .filter_by(department_id=department_id)
+            .all()
+        )
+        put_database_cache_entry(*cache_params, officers)
