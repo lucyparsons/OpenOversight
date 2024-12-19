bd2971f9c5bd74d0c22b975e0e863c6974ceb10f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-12-19 14:34:13 -0600

    Update test_api_v1.py

--- OpenOversight/tests/routes/test_api_v1.py
+++ OpenOversight/tests/routes/test_api_v1.py
@@ -1,11 +1,12 @@
 import pytest
 from flask import current_app, url_for
 
-@pytest.mark.parametrize(
-    "department_id",
-    [1, 3000]
-)
+
+@pytest.mark.parametrize("department_id", [1, 3000])
 def test_get_dept_officers(client, department_id: int):
     with current_app.test_request_context():
-        resp = client.get(url_for("v1.get_dept_officers", department_id=department_id), follow_recirects=False)
+        resp = client.get(
+            url_for("v1.get_dept_officers", department_id=department_id),
+            follow_recirects=False,
+        )
         print(resp)
