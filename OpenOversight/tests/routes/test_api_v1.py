f73b203b89327f736981ad7b554fcc599b63fab0
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-12-19 14:42:48 -0600

    Update test_api_v1.py

--- OpenOversight/tests/routes/test_api_v1.py
+++ OpenOversight/tests/routes/test_api_v1.py
@@ -1,12 +1,23 @@
+import json
+from http import HTTPStatus
+
 import pytest
 from flask import current_app, url_for
 
+from OpenOversight.app.models.database import Officer
+from OpenOversight.app.utils.constants import ENCODING_UTF_8
+
 
 @pytest.mark.parametrize("department_id", [1, 3000])
-def test_get_dept_officers(client, department_id: int):
+def test_get_dept_officers(client, session, department_id: int):
     with current_app.test_request_context():
+        expected_offices = Officer.query.filter_by(department_id=department_id).all()
+
         resp = client.get(
             url_for("v1.get_dept_officers", department_id=department_id),
             follow_recirects=False,
         )
-        print(resp)
+        officers = json.loads(resp.data.decode(ENCODING_UTF_8))
+
+        assert resp.status_code == HTTPStatus.OK
+        assert len(officers) == len(expected_offices)
