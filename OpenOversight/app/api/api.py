5a39961d1de15eb1ae03b4c1bcceb0cbc65cb458
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-12-19 14:21:09 -0600

    Restructure

--- OpenOversight/app/api/api.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from OpenOversight.app.api.v1.api import v1 as v1_blueprint
-
-
-api = Blueprint("api", __name__, url_prefix="/api")
-
-api.register_blueprint(v1_blueprint)
