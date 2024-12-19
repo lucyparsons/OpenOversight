1efde38954ffe04a934876b4a75c4ee99fad43d6
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-12-19 15:37:13 -0600

    Update v1.py

--- OpenOversight/app/api/v1.py
+++ OpenOversight/app/api/v1.py
@@ -4,14 +4,27 @@ from http import HTTPMethod, HTTPStatus
 from flask import Blueprint, current_app
 from sqlalchemy.orm import contains_eager, joinedload
 
-from OpenOversight.app.models.database import Assignment, Department, Officer, db
+from OpenOversight.app.models.database import (
+    Assignment,
+    Department,
+    Description,
+    Incident,
+    Link,
+    Officer,
+    Salary,
+    db,
+)
 from OpenOversight.app.models.database_cache import (
     get_database_cache_entry,
     put_database_cache_entry,
 )
 from OpenOversight.app.utils.constants import (
     KEY_DEPT_ALL_ASSIGNMENTS,
+    KEY_DEPT_ALL_INCIDENTS,
+    KEY_DEPT_ALL_LINKS,
+    KEY_DEPT_ALL_NOTES,
     KEY_DEPT_ALL_OFFICERS,
+    KEY_DEPT_ALL_SALARIES,
 )
 from OpenOversight.app.utils.flask import limiter
 
@@ -24,6 +37,7 @@ v1 = Blueprint("v1", __name__, url_prefix="/api/v1")
 def get_dept_officers(department_id: int):
     cache_params = (Department(id=department_id), KEY_DEPT_ALL_OFFICERS)
     officers = get_database_cache_entry(*cache_params)
+
     if officers is None:
         officers = (
             db.session.query(Officer)
@@ -46,6 +60,7 @@ def get_dept_officers(department_id: int):
 def get_dept_assignments(department_id: int):
     cache_params = Department(id=department_id), KEY_DEPT_ALL_ASSIGNMENTS
     assignments = get_database_cache_entry(*cache_params)
+
     if assignments is None:
         assignments = (
             db.session.query(Assignment)
@@ -61,5 +76,91 @@ def get_dept_assignments(department_id: int):
     return current_app.response_class(
         mimetype="application/json",
         response=json.dumps(assignments),
+        status=HTTPStatus.OK,
+    )
+
+
+@v1.route("/departments/<int:department_id>/incidents", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_incidents(department_id: int):
+    cache_params = (Department(id=department_id), KEY_DEPT_ALL_INCIDENTS)
+    incidents = get_database_cache_entry(*cache_params)
+
+    if incidents is None:
+        incidents = Incident.query.filter_by(department_id=department_id).all()
+        put_database_cache_entry(*cache_params, incidents)
+
+    return current_app.response_class(
+        mimetype="application/json",
+        response=json.dumps(incidents),
+        status=HTTPStatus.OK,
+    )
+
+
+@v1.route("/departments/<int:department_id>/salaries", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_salaries(department_id: int):
+    cache_params = (Department(id=department_id), KEY_DEPT_ALL_SALARIES)
+    salaries = get_database_cache_entry(*cache_params)
+
+    if salaries is None:
+        salaries = (
+            db.session.query(Salary)
+            .join(Salary.officer)
+            .filter(Officer.department_id == department_id)
+            .options(contains_eager(Salary.officer))
+            .all()
+        )
+        put_database_cache_entry(*cache_params, salaries)
+
+    return current_app.response_class(
+        mimetype="application/json",
+        response=json.dumps(salaries),
+        status=HTTPStatus.OK,
+    )
+
+
+@v1.route("/departments/<int:department_id>/links", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_links(department_id: int):
+    cache_params = (Department(id=department_id), KEY_DEPT_ALL_LINKS)
+    links = get_database_cache_entry(*cache_params)
+
+    if links is None:
+        links = (
+            db.session.query(Link)
+            .join(Link.officers)
+            .filter(Officer.department_id == department_id)
+            .options(contains_eager(Link.officers))
+            .all()
+        )
+        put_database_cache_entry(*cache_params, links)
+
+    return current_app.response_class(
+        mimetype="application/json",
+        response=json.dumps(links),
+        status=HTTPStatus.OK,
+    )
+
+
+@v1.route("/departments/<int:department_id>/descriptions", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_descriptions(department_id: int):
+    cache_params = (Department(id=department_id), KEY_DEPT_ALL_NOTES)
+    notes = get_database_cache_entry(*cache_params)
+
+    if notes is None:
+        notes = (
+            db.session.query(Description)
+            .join(Description.officer)
+            .filter(Officer.department_id == department_id)
+            .options(contains_eager(Description.officer))
+            .all()
+        )
+        put_database_cache_entry(*cache_params, notes)
+
+    return current_app.response_class(
+        mimetype="application/json",
+        response=json.dumps(notes),
         status=HTTPStatus.OK,
     )
