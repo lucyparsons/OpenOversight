c7a1f64b651a2c833d9182cede3c8d4e4674f273
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-01-19 01:32:13 -0600

    Update v1.py

--- OpenOversight/app/api/v1.py
+++ OpenOversight/app/api/v1.py
@@ -14,18 +14,18 @@ def objs_to_dicts_jsonify(obj_list: Query) -> Response:
     return jsonify([o.to_dict() for o in obj_list])
 
 
-@v1.route("/departments/<int:department_id>/officers", methods=[HTTPMethod.GET])
-@limiter.limit("5/minute")
-def get_dept_officers(department_id: int) -> Response:
-    officers = Department.get_officers(department_id)
-    return objs_to_dicts_jsonify(officers)
-
-
 @v1.route("/departments/<int:department_id>/assignments", methods=[HTTPMethod.GET])
 @limiter.limit("5/minute")
 def get_dept_assignments(department_id: int) -> Response:
     assignments = Department.get_assignments(department_id)
     return objs_to_dicts_jsonify(assignments)
+
+
+@v1.route("/departments/<int:department_id>/descriptions", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_descriptions(department_id: int) -> Response:
+    descriptions = Department.get_descriptions(department_id)
+    return objs_to_dicts_jsonify(descriptions)
 
 
 @v1.route("/departments/<int:department_id>/incidents", methods=[HTTPMethod.GET])
@@ -35,13 +35,6 @@ def get_dept_incidents(department_id: int) -> Response:
     return objs_to_dicts_jsonify(incidents)
 
 
-@v1.route("/departments/<int:department_id>/salaries", methods=[HTTPMethod.GET])
-@limiter.limit("5/minute")
-def get_dept_salaries(department_id: int) -> Response:
-    salaries = Department.get_salaries(department_id)
-    return objs_to_dicts_jsonify(salaries)
-
-
 @v1.route("/departments/<int:department_id>/links", methods=[HTTPMethod.GET])
 @limiter.limit("5/minute")
 def get_dept_links(department_id: int) -> Response:
@@ -49,8 +42,15 @@ def get_dept_links(department_id: int) -> Response:
     return objs_to_dicts_jsonify(links)
 
 
-@v1.route("/departments/<int:department_id>/descriptions", methods=[HTTPMethod.GET])
+@v1.route("/departments/<int:department_id>/officers", methods=[HTTPMethod.GET])
 @limiter.limit("5/minute")
-def get_dept_descriptions(department_id: int) -> Response:
-    descriptions = Department.get_descriptions(department_id)
-    return objs_to_dicts_jsonify(descriptions)
+def get_dept_officers(department_id: int) -> Response:
+    officers = Department.get_officers(department_id)
+    return objs_to_dicts_jsonify(officers)
+
+
+@v1.route("/departments/<int:department_id>/salaries", methods=[HTTPMethod.GET])
+@limiter.limit("5/minute")
+def get_dept_salaries(department_id: int) -> Response:
+    salaries = Department.get_salaries(department_id)
+    return objs_to_dicts_jsonify(salaries)
