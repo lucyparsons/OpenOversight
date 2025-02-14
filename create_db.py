2cd6f9a71f8225a45109b3fec49c2d90ab41646e
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-02-14 12:28:11 -0600

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
 from OpenOversight.app import create_app
 from OpenOversight.app.models.database import db
+from OpenOversight.app.utils.constants import KEY_ENV_DEV
 
 
-app = create_app("development")
+app = create_app(KEY_ENV_DEV)
 with app.app_context():
     db.app = app
     db.create_all()
