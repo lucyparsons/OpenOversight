lucyparsons/OpenOversight

commit 7bf96698ca7b6604f1ac17946ec61173db4ea067

UX Re-design (#1155)

OpenOversight/app/main/forms.py
@@ -103,7 +103,7 @@ class FindOfficerForm(Form):
         "dept",
         validators=[DataRequired()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
     unit = StringField("unit", default="Not Sure", validators=[Optional()])
     current_job = BooleanField("current_job", default=None, validators=[Optional()])
@@ -122,12 +122,8 @@ class FindOfficerForm(Form):
         choices=GENDER_CHOICES,
         validators=[AnyOf(allowed_values(GENDER_CHOICES))],
     )
-    min_age = IntegerField(
-        "min_age", default=16, validators=[NumberRange(min=16, max=100)]
-    )
-    max_age = IntegerField(
-        "max_age", default=85, validators=[NumberRange(min=16, max=100)]
-    )
+    min_age = IntegerField("min_age", validators=[NumberRange(min=16, max=100)])
+    max_age = IntegerField("max_age", validators=[NumberRange(min=16, max=100)])
     require_photo = BooleanField(
         "require_photo", default=False, validators=[Optional()]
     )
@@ -285,7 +281,7 @@ class AddOfficerForm(Form):
         "Department",
         validators=[DataRequired()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
     first_name = StringField(
         "First name",
@@ -407,7 +403,7 @@ class EditOfficerForm(Form):
         "Department",
         validators=[Optional()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
     submit = SubmitField(label="Update")
 
@@ -422,7 +418,7 @@ class AddUnitForm(Form):
         "Department",
         validators=[DataRequired()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
     submit = SubmitField(label="Add")
 
@@ -432,7 +428,7 @@ class AddImageForm(Form):
         "Department",
         validators=[DataRequired()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
 
 
@@ -533,7 +529,7 @@ class IncidentForm(DateFieldForm):
         "Department*",
         validators=[DataRequired()],
         query_factory=dept_choices,
-        get_label="display_name",
+        get_label="name",
     )
     address = FormField(LocationForm)
     officers = FieldList(

commit e2adc6b3323f32dabe5d1159791b750b71d98c73

Allow /find form to handle omitted age range. (#1162)
## Fixes issue
An issue was introduced in #1155, where the default values for the age
fields in the `FindOfficerForm` were removed. This leads to validation
errors when submitting the form without setting an age.
This can be verified here: https://staging.openoversight.com/find
(pressing "Generate Officer Gallery" will show the validation error)


![find-form-age-bug](https://github.com/user-attachments/assets/142dda48-b5b7-429e-8f21-35e67d1a1b96)

 

## Description of Changes

Adding the `Optional()` validator will prevent this issue.

## Tests and Linting
- [x] This branch is up-to-date with the `develop` branch.
- [ ] Ran `make create_db_diagram` command.
- [x] `pytest` passes on my local development environment.
- [x] `pre-commit` passes on my local development environment.

OpenOversight/app/main/forms.py
@@ -122,8 +122,12 @@ class FindOfficerForm(Form):
         choices=GENDER_CHOICES,
         validators=[AnyOf(allowed_values(GENDER_CHOICES))],
     )
-    min_age = IntegerField("min_age", validators=[NumberRange(min=16, max=100)])
-    max_age = IntegerField("max_age", validators=[NumberRange(min=16, max=100)])
+    min_age = IntegerField(
+        "min_age", validators=[Optional(), NumberRange(min=16, max=100)]
+    )
+    max_age = IntegerField(
+        "max_age", validators=[Optional(), NumberRange(min=16, max=100)]
+    )
     require_photo = BooleanField(
         "require_photo", default=False, validators=[Optional()]
     )