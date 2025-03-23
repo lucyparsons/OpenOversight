fe145b528ff56b22da1ef6192f5b190704324f9f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-22 20:57:10 -0500

    Update views.py

--- OpenOversight/app/main/views.py
+++ OpenOversight/app/main/views.py
@@ -143,8 +143,8 @@ def redirect_url(default="main.index"):
 
 
 @sitemap_include
-@main.route("/")
-@main.route("/index")
+@main.route("/", methods=[HTTPMethod.GET])
+@main.route("/index", methods=[HTTPMethod.GET])
 def index():
     departments_by_state = Department.by_state()
     department_count = sum(
