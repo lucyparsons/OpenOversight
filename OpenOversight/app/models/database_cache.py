4523a3d081e3b943c55379ed3291f90addaec882
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-25 17:19:39 -0500

    Update database_cache.py

--- OpenOversight/app/models/database_cache.py
+++ OpenOversight/app/models/database_cache.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, Hashable, List
 
 from cachetools import TTLCache
 from cachetools.keys import hashkey
@@ -10,7 +10,7 @@ from OpenOversight.app.utils.constants import HOUR
 DB_CACHE = TTLCache(maxsize=1024, ttl=24 * HOUR)
 
 
-def get_model_cache_key(model: Model, update_type: str) -> Any:
+def get_model_cache_key(model: Model, update_type: str) -> tuple[Hashable]:
     """Create unique db.Model key."""
     if model is not None:
         return hashkey(model.id, update_type, model.__class__.__name__)
